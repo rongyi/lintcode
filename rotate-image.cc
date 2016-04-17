44cb447c08845a184267ad2e8bb75990322c1187
rongyi <TODO>
2016-04-17 22:41:55 +0800

    Add rotate image

---
+++ rotate-image.cc
@@ -0,0 +1,26 @@
+// http://www.lintcode.com/zh-cn/problem/rotate-image
+
+class Solution {
+public:
+  /**
+   * @param matrix: A list of lists of integers
+   * @return: Void
+   */
+  void rotate(vector<vector<int> > &matrix) {
+    const int size = matrix.size();
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size - i; j++) {
+        int tmp = matrix[i][j];
+        matrix[i][j] = matrix[size - j - 1][size - i - 1];
+        matrix[size - j - 1][size - i - 1] = tmp;
+      }
+    }
+    for (int i = 0; i < size / 2; i++) {
+      for (int j = 0; j < size; j++) {
+        int tmp = matrix[i][j];
+        matrix[i][j] = matrix[size - i - 1][j];
+        matrix[size -i - 1][j] = tmp;
+      }
+    }
+  }
+};

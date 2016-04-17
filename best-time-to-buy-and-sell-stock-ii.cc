6d55d68931ae9b64e6ece0c3835c0c2476a4f638
rongyi <TODO>
2016-04-17 22:43:22 +0800

    Add best time to buy and sell stock

---
+++ best-time-to-buy-and-sell-stock-ii.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/best-time-to-buy-and-sell-stock-ii
+
+class Solution {
+public:
+  /**
+   * @param prices: Given an integer array
+   * @return: Maximum profit
+   */
+  int maxProfit(vector<int> &prices) {
+    if (prices.size() == 0)
+      return 0;
+    int profit = 0;
+    for (int i = 0; i < prices.size() - 1; i++) {
+      if (prices[i] < prices[i + 1]) {
+        profit += prices[i + 1] - prices[i];
+      }
+    }
+
+    return profit;
+  }
+};

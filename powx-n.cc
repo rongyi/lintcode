b8d83e2dbc8bc0ae321ffd03486a8cb3354372fc
rongyi <TODO>
2016-04-17 22:05:43 +0800

    Add power n

---
+++ powx-n.cc
@@ -0,0 +1,24 @@
+//http://www.lintcode.com/zh-cn/problem/powx-n/
+
+class Solution {
+public:
+  /**
+   * @param x the base number
+   * @param n the power number
+   * @return the result
+   */
+  double myPow(double x, int n) {
+    // Write your code here
+    if (x == 0)
+      return 0;
+    if (n < 0)
+      return 1 / myPow(x, -n);
+    if (n == 0)
+      return 1;
+    if (n & 0x1) {
+      return x * myPow(x, n - 1);
+    } else {
+      return myPow(x, n / 2) * myPow(x, n / 2);
+    }
+  }
+};

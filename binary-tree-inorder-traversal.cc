4bb234d8f25c46f0531037e8a624b04f02611d6e
rongyi <TODO>
2016-04-17 23:04:25 +0800

    Add bst in order traversal

---
+++ binary-tree-inorder-traversal.cc
@@ -0,0 +1,36 @@
+// http://www.lintcode.com/zh-cn/problem/binary-tree-inorder-traversal
+
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+#include <vector>
+using namespace std;
+class Solution {
+  /**
+   * @param root: The root of binary tree.
+   * @return: Inorder in vector which contains node values.
+   */
+public:
+  vector<int> inorderTraversal(TreeNode *root) {
+    std::vector<int> ret;
+    inner(root, ret);
+    return ret;
+  }
+private:
+  void inner(TreeNode *root, std::vector<int> &ret) {
+    if (root) {
+      inner(root->left, ret);
+      ret.push_back(root->val);
+      inner(root->right, ret);
+    }
+  }
+};

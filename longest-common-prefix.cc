77f1457bde792c8ff88d137af67ef0d04518610d
rongyi <TODO>
2016-04-17 23:00:36 +0800

    Add longest common prefix

---
+++ longest-common-prefix.cc
@@ -0,0 +1,22 @@
+// http://www.lintcode.com/zh-cn/problem/longest-common-prefix
+
+class Solution {
+public:
+  /**
+   * @param strs: A list of strings
+   * @return: The longest common prefix
+   */
+  string longestCommonPrefix(vector<string> &strs) {
+    // write your code here
+    if (strs.empty())
+      return "";
+    for (unsigned i = 0; i < strs[0].size(); ++i) {
+      for (unsigned j = 1; j < strs.size(); ++j) {
+        if (strs[j][i] != strs[0][i])
+          return strs[0].substr(0, i);
+      }
+    }
+    return strs[0];
+
+  }
+};

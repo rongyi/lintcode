0dd33cca16671f0ed7c817ca8298bcc951789c63
rongyi <TODO>
2016-04-17 22:05:08 +0800

    Add longest words

---
+++ longest-words.cc
@@ -0,0 +1,25 @@
+// http://www.lintcode.com/zh-cn/problem/longest-words/
+
+class Solution {
+public:
+  /**
+   * @param dictionary: a vector of strings
+   * @return: a vector of strings
+   */
+  vector<string> longestWords(vector<string> &dictionary) {
+    vector<string> ret;
+    int max_len = 0;
+    for (int i = 0; i < dictionary.size(); i++) {
+      const std::string &cur = dictionary[i];
+      if (cur.length() == max_len) {
+        // max_len = cur.length();
+        ret.push_back(cur);
+      } else if (cur.length() > max_len){
+        ret.clear();
+        max_len = cur.length();
+        ret.push_back(cur);
+      }
+    }
+    return ret;
+  }
+};

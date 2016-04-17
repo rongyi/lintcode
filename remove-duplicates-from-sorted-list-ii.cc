22497d21608b2a2e3d75062e6efa4d7b85a33d0b
rongyi <TODO>
2016-04-17 22:47:15 +0800

    Add remove duplicates

---
+++ remove-duplicates-from-sorted-list-ii.cc
@@ -0,0 +1,39 @@
+// http://www.lintcode.com/zh-cn/problem/remove-duplicates-from-sorted-list-ii
+
+/**
+ * Definition of ListNode
+ * class ListNode {
+ * public:
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int val) {
+ *         this->val = val;
+ *         this->next = NULL;
+ *     }
+ * }
+ */
+class Solution{
+public:
+  /**
+   * @param head: The first node of linked list.
+   * @return: head node
+   */
+  ListNode * deleteDuplicates(ListNode *head) {
+    if (!head || !head->next)
+      return head;
+
+    ListNode *p = head->next;
+    if (head->val == p->val) {
+      while (p && head->val == p->val) {
+        ListNode *tmp = p;
+        p = p->next;
+        delete tmp;
+      }
+      delete head; // clear all
+      return deleteDuplicates(p);
+    } else {
+      head->next = deleteDuplicates(head->next);
+      return head;
+    }
+  }
+};

9c6087f15244df79a494f758d4859a9fb0e3004b
rongyi <TODO>
2016-04-17 22:13:06 +0800

    Add firstWillWin

---
+++ coins-in-a-line.cc
@@ -0,0 +1,12 @@
+// http://www.lintcode.com/zh-cn/problem/coins-in-a-line/
+
+class Solution {
+public:
+  /**
+   * @param n: an integer
+   * @return: a boolean which equals to true if the first player will win
+   */
+  bool firstWillWin(int n) {
+    return n % 3 != 0;
+  }
+};

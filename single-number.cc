59314a8d573e08c39967b1b44ec8edef80838d53
rongyi <TODO>
2016-04-17 23:00:06 +0800

    Add single number

---
+++ single-number.cc
@@ -0,0 +1,18 @@
+// http://www.lintcode.com/zh-cn/problem/single-number
+
+class Solution {
+public:
+  /**
+   * @param A: Array of integers.
+   * return: The single number.
+   */
+  int singleNumber(vector<int> &A) {
+    int ret = 0;
+    for (vector<int>::const_iterator iter = A.begin();
+         iter != A.end();
+         iter++) {
+      ret ^= (*iter);
+    }
+    return ret;
+  }
+};

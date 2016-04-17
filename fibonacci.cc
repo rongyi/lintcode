f30461b90bc84b3a000fbacfe2fa3986e6b4a79a
rongyi <TODO>
2016-04-17 22:11:56 +0800

    Add fibonacci

---
+++ fibonacci.cc
@@ -0,0 +1,19 @@
+// http://www.lintcode.com/zh-cn/problem/fibonacci/
+
+class Solution{
+public:
+  /**
+   * @param n: an integer
+   * @return an integer f(n)
+   */
+  int fibonacci(int n) {
+    int a = 0;
+    int b = 1;
+    for (int i = 1; i < n; i++) {
+      b = a + b;
+      a = b - a;
+    }
+
+    return a;
+  }
+};

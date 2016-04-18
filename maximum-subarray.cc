97259b353da88887571da38be6097a68d917b690
rongyi <sqrongyi@163.com>
2016-04-18 09:11:00 +0800

    Add maximum subarray

---
+++ maximum-subarray.cc
@@ -0,0 +1,23 @@
+// http://www.lintcode.com/zh-cn/problem/maximum-subarray
+
+class Solution {
+public:
+  /**
+   * @param nums: A list of integers
+   * @return: A integer indicate the sum of max subarray
+   */
+  int maxSubArray(vector<int> nums) {
+    if (!nums.size())
+      return 0;
+    const int n = nums.size();
+    int cur_sum = 0;
+    int max_sum = nums[0];
+
+    for (int i = 0; i < n; i++) {
+      cur_sum = (nums[i] > cur_sum + nums[i]) ? nums[i] : cur_sum + nums[i];
+      if (cur_sum > max_sum)
+        max_sum = cur_sum;
+    }
+    return max_sum;
+  }
+};

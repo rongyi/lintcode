b5997918b8574924458fb5a0827ee859edceadfa
rongyi <sqrongyi@163.com>
2016-04-18 09:10:15 +0800

    Add majority number

---
+++ majority-number.cc
@@ -0,0 +1,27 @@
+// http://www.lintcode.com/zh-cn/problem/majority-number
+
+#include <map>
+class Solution {
+public:
+  /**
+   * @param nums: A list of integers
+   * @return: The majority number
+   */
+  int majorityNumber(vector<int> nums) {
+    const unsigned size = nums.size();
+    std::map<int, int> count_map;
+    for (int i = 0; i < size; i++) {
+      if (count_map.find(nums[i]) == count_map.end())
+        count_map[nums[i]] = 1;
+      else
+        count_map[nums[i]]++;
+    }
+    for (std::map<int, int>::const_iterator iter = count_map.begin();
+         iter != count_map.end();
+         iter++) {
+      if (iter->second > size / 2)
+        return iter->first;
+    }
+    return -1;
+  }
+};

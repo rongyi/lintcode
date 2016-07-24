d294d376baba299d813bb2ba524717d7aee736e8
rongyi <sqrongyi@163.com>
2016-07-24 14:32:18 +0800

    Add more code

---
+++ segment-tree-build.cc
@@ -0,0 +1,42 @@
+// http://www.lintcode.com/zh-cn/problem/segment-tree-build
+#include <vector>
+#include <iostream>
+
+using std::vector;
+using std::cout;
+using std::endl;
+
+
+class SegmentTreeNode {
+public:
+    int start, end;
+    SegmentTreeNode *left, *right;
+    SegmentTreeNode(int start, int end) {
+        this->start = start, this->end = end;
+        this->left = this->right = NULL;
+    }
+};
+
+
+class Solution {
+public:
+  /**
+   *@param start, end: Denote an segment / interval
+   *@return: The root of Segment Tree
+   */
+  SegmentTreeNode * build(int start, int end) {
+    if (start > end) {
+      return nullptr;
+    }
+    if (start == end) {
+      return new SegmentTreeNode(start, end);
+    }
+    SegmentTreeNode *root = new SegmentTreeNode(start, end);
+    const int mid = start + (end - start) / 2;
+    root->left = build(start, mid);
+    root->right = build(mid + 1, end);
+
+    return root;
+  }
+
+};

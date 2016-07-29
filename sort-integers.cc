1a15f65ae8a33966f2d19bbe16444c3191e788c2
rongyi <sqrongyi@163.com>
2016-07-29 11:21:24 +0800

    Add raw sort

---
+++ sort-integers.cc
@@ -0,0 +1,46 @@
+// http://www.lintcode.com/zh-cn/problem/sort-integers
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param A an integer array
+   * @return void
+   */
+  void sortIntegers(vector<int>& A) {
+    if (A.empty())
+      return;
+    const int len = A.size();
+    for (int i = 0; i < len; i++) {
+      int min = A[i];
+      int min_index = i;
+      for (int j = i + 1; j < len; j++) {
+        if (A[j] < min) {
+          min = A[j];
+          min_index = j;
+        }
+      }
+      if (min_index != i) {
+        std::swap(A[i], A[min_index]);
+      }
+    }
+  }
+};
+
+int main()
+{
+  vector<int> test{3, 2, 1, 4, 5};
+  Solution so;
+  so.sortIntegers(test);
+  for (auto i : test)
+    cout << i << " ";
+  cout << endl;
+  return 0;
+}

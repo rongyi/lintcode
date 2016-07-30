13cbb504f1af83ca91b2688b4ded4443006397d8
rongyi <sqrongyi@163.com>
2016-07-30 22:25:21 +0800

    Add minimum sum

---
+++ minimum-subarray.cc
@@ -0,0 +1,35 @@
+// http://www.lintcode.com/zh-cn/problem/minimum-subarray
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param nums: a list of integers
+   * @return: A integer denote the sum of minimum subarray
+   */
+  int minSubArray(vector<int> nums) {
+    int ret = INT_MAX;
+    int sum = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      if (sum > 0) {
+        sum = nums[i];
+        if (ret > sum)
+          ret = sum;
+      } else {
+        sum += nums[i];
+        if (ret > sum)
+          ret = sum;
+      }
+    }
+
+    return ret;
+  }
+};

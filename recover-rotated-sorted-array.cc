02d67e79e548b2e15009dae52990c1f3a96552a5
rongyi <sqrongyi@163.com>
2016-08-01 20:09:26 +0800

    Update, add recover rotated array

---
+++ recover-rotated-sorted-array.cc
@@ -0,0 +1,42 @@
+// http://www.lintcode.com/zh-cn/problem/recover-rotated-sorted-array
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  void recoverRotatedSortedArray(vector<int> &nums) {
+    if (nums.empty())
+      return;
+    const int n = nums.size();
+    int i = 0;
+    for (; i < n - 1; i++) {
+      // find the pivot
+      if (nums[i] > nums[i + 1]) {
+        break;
+      }
+    }
+    for (int j = 0; j <= i; j++) {
+      nums.push_back(nums[j]);
+    }
+    nums.erase(nums.begin(), nums.begin() + i + 1);
+
+    // for (auto i: nums)
+    //   cout << i << " ";
+    // cout << endl;
+  }
+};
+
+int main()
+{
+  Solution so;
+  vector<int> test{4, 1, 2, 3};
+  so.recoverRotatedSortedArray(test);
+
+  return 0;
+}

9bfbcefd4b5bc946bdc8b778794cc89bf2c34ce1
rongyi <sqrongyi@163.com>
2016-08-01 16:20:54 +0800

    Update local

---
+++ reverse-integer.cc
@@ -0,0 +1,43 @@
+// http://www.lintcode.com/zh-cn/problem/reverse-integer
+#include <vector>
+#include <iostream>
+#include <string>
+#include <climits>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param n the integer to be reversed
+   * @return the reversed integer
+   */
+  int reverseInteger(int n) {
+    int sign = 1;
+    int ret = 0;
+    if (n < 0) {
+      sign = -1;
+      n *= -1;
+    }
+    while (n) {
+      int tmp = ret * 10 + n % 10;
+      if (tmp / 10 != ret)
+        return 0;
+      ret = tmp;
+      n /= 10;
+    }
+
+    return ret * sign;
+  }
+};
+
+int main()
+{
+  Solution so;
+  auto ret = so.reverseInteger(0);
+  cout << ret << endl;
+  return 0;
+}

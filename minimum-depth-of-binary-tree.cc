20dde5026c4bc6fc520cc53f063d0306693dbd19
rongyi <sqrongyi@163.com>
2016-08-02 14:19:47 +0800

    Update minimum depth of binary tree

---
+++ minimum-depth-of-binary-tree.cc
@@ -0,0 +1,49 @@
+// http://www.lintcode.com/zh-cn/problem/minimum-depth-of-binary-tree
+#include <vector>
+#include <iostream>
+#include <string>
+#include <climits>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int val) {
+        this->val = val;
+        this->left = this->right = NULL;
+    }
+};
+
+class Solution {
+public:
+  /**
+   * @param root: The root of binary tree.
+   * @return: An integer
+   */
+  int do_min(TreeNode *root) {
+    if (!root)
+      return 0;
+    if (root->left == nullptr && root->right == nullptr)
+      return 1;
+    int left_height = INT_MAX;
+    int right_height = INT_MAX;
+    if (root->left)
+      left_height = do_min(root->left);
+    if (root->right)
+      right_height = do_min(root->right);
+
+    return std::min(left_height, right_height) + 1;
+  }
+
+  int minDepth(TreeNode *root) {
+    if (!root)
+      return 0;
+    return do_min(root);
+  }
+};

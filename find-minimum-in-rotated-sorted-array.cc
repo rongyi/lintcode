b6a69ec01a1e2832ce4e8079c8296dadbdf92e99
rongyi <sqrongyi@163.com>
2016-08-02 09:45:19 +0800

    Add rotated sorted array

---
+++ find-minimum-in-rotated-sorted-array.cc
@@ -0,0 +1,52 @@
+// http://www.lintcode.com/zh-cn/problem/find-minimum-in-rotated-sorted-array
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param num: a rotated sorted array
+   * @return: the minimum number in the array
+   */
+  int findMin(vector<int> &nums) {
+    if (nums.empty())
+      return -1;
+    // const int n = nums.size();
+    int start = 0;
+    int end = nums.size() - 1;
+
+    while (start <= end) {
+      const int mid = start + (end - start) / 2;
+      // in regular
+      if (nums[start] <= nums[end]) {
+        break;
+      } else {
+        // not in a rotated list
+        if (nums[start] <= nums[mid] && nums[mid] >= nums[end]) {
+          start = mid + 1;
+        } else if (nums[start] >= nums[mid] && nums[start] >= nums[end]) {
+          start += 1;
+        } else {
+          end = mid - 1;
+        }
+      }
+    }
+
+    return nums[start];
+  }
+};
+
+int main()
+{
+  Solution so;
+  vector<int> test{1, 2, 3};
+  auto ret = so.findMin(test);
+  cout << ret << endl;
+  return 0;
+}

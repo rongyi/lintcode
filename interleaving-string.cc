628aa71fe392bf07058095df406eb903336c033b
rongyi <sqrongyi@163.com>
2016-08-03 14:55:05 +0800

    Add cross template

---
+++ interleaving-string.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/interleaving-string
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * Determine whether s3 is formed by interleaving of s1 and s2.
+   * @param s1, s2, s3: As description.
+   * @return: true of false.
+   */
+  bool isInterleave(string s1, string s2, string s3) {
+    // write your code here
+  }
+};

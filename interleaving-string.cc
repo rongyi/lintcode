83167b0ade6a32b1e51d8817968291dd58ecac1c
rongyi <sqrongyi@163.com>
2016-08-04 15:48:19 +0800

    Interleaving before count

--- interleaving-string.cc
+++ interleaving-string.cc
@@ -16,6 +16,36 @@ public:
    * @return: true of false.
    */
   bool isInterleave(string s1, string s2, string s3) {
-    // write your code here
+
+    const int len1 = s1.size();
+    const int len2 = s2.size();
+
+    if (len1 + len2 != s3.size())
+      return false;
+
+    vector<vector<bool>> ret(len1 + 1, vector<bool>(len2 + 1, false));
+
+    ret[0][0] = true;
+
+
+    for (int i = 1; i <= len1; i++) {
+      if (s1[i - 1] == s3[i - 1])
+        ret[i][0] = true;
+    }
+
+    for (int j = 1; j <= len2; j++) {
+      if (s2[j - 1] == s3[j - 1])
+        ret[0][j] = true;
+    }
+
+
+    for (int i = 0; i <= len1; i++) {
+
+      for (int j = 1; j <= len2; j++) {
+        ret[i][j] = (ret[i - 1][j] && s1[i - 1] == s3[i + j - 1]) ||
+          (ret[i][j - 1] && s2[j - 1] == s3[i + j - 1]);
+      }
+    }
+    return ret[len1][len2];
   }
 };

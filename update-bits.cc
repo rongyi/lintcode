473c0051f83b0f746d7d45ab9e661619c7f6f495
rongyi <sqrongyi@163.com>
2016-08-05 15:11:28 +0800

    Add update bits

--- update-bits.cc
+++ update-bits.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <bitset>
 
 using std::vector;
 using std::cout;
@@ -16,6 +17,28 @@ public:
    *return: An integer
    */
   int updateBits(int n, int m, int i, int j) {
-    // write your code here
+    if (i > j)
+      return -1;
+
+    for (int ni = i, k = 0; ni <= j; ni++, k++) {
+      // first clear the bit
+      n &= ~(1 << ni);
+      // then add m
+      int x = (m & (1 << k)) >> k;
+      n |= (x << ni);
+    }
+
+    return n;
   }
 };
+
+int main()
+{
+  auto a = 1024;
+  auto b = 21;
+  Solution so;
+  auto ret = so.updateBits(a, b, 2, 6);
+  cout << ret << endl;
+
+  return 0;
+}

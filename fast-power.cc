a5732c8d2ca4178e2903da448f8e9dae3367c480
rongyi <sqrongyi@163.com>
2016-08-05 15:43:26 +0800

    Add fast power

---
+++ fast-power.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/fast-power
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /*
+   * @param a, b, n: 32bit integers
+   * @return: An integer
+   */
+  int fastPower(int a, int b, int n) {
+    // write your code here
+  }
+};

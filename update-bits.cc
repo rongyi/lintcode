0b8114e8e75cfc1bda873e2dcf05d0aa9ede92a3
rongyi <sqrongyi@163.com>
2016-08-05 14:27:17 +0800

    Add update bits template

---
+++ update-bits.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/update-bits
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   *@param n, m: Two integer
+   *@param i, j: Two bit positions
+   *return: An integer
+   */
+  int updateBits(int n, int m, int i, int j) {
+    // write your code here
+  }
+};

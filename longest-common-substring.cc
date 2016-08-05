28188b986cbe230d3a6da79b20c03e2ee1273483
rongyi <sqrongyi@163.com>
2016-08-05 15:30:47 +0800

    Add longestCommonSubstring

---
+++ longest-common-substring.cc
@@ -0,0 +1,11 @@
+// http://www.lintcode.com/zh-cn/problem/longest-common-substring
+class Solution {
+public:
+  /**
+   * @param A, B: Two string.
+   * @return: the length of the longest common substring.
+   */
+  int longestCommonSubstring(string &A, string &B) {
+    // write your code here
+  }
+};

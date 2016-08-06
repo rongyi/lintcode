d79d0af6e8767fab2395ef194fb80d1f87ecdf2b
rongyi <sqrongyi@163.com>
2016-08-06 10:21:51 +0800

    count and say pass

--- count-and-say.cc
+++ count-and-say.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using std::vector;
 using std::cout;
@@ -15,6 +16,32 @@ public:
    * @return the nth sequence
    */
   string countAndSay(int n) {
-    // Write your code here
+    string seq = "1";
+
+    for (int i = 1; i < n; i++) {
+      std::stringstream ss;
+      char last = seq[0];
+      int count = 0;
+      for (int j = 0; j <= seq.size(); j++) {
+        if (seq[j] == last) {
+          count++;
+          continue;
+        } else {
+          ss << count << last;
+          last = seq[j];
+          count = 1;
+        }
+      }
+      seq = ss.str();
+    }
+    return seq;
   }
 };
+
+int main()
+{
+  Solution so;
+  auto ret = so.countAndSay(5);
+  cout << ret << endl;
+  return 0;
+}

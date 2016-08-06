0f6182d0db0289fd7121d7e067f757e92bd4b91d
rongyi <sqrongyi@163.com>
2016-08-06 16:09:32 +0800

    Add wood cut

---
+++ wood-cut.cc
@@ -0,0 +1,52 @@
+// http://www.lintcode.com/zh-cn/problem/wood-cut
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   *@param L: Given n pieces of wood with length L[i]
+   *@param k: An integer
+   *return: The maximum length of the small pieces.
+   */
+  int woodCut(vector<int> L, int k) {
+    if (L.empty() || k == 0)
+      return false;
+    int begin = 0;
+    // end is the max of vector L
+    int end = 0;
+    int ret = 0;
+    for (int i = 0; i < L.size(); i++) {
+      end = std::max(end, L[i]);
+    }
+
+    while (begin <= end) {
+      const int mid = begin + (end - begin) / 2;
+      // can be much bigger
+      if (unit_qulify(L, mid, k)) {
+        begin = mid + 1;
+        ret = mid;
+      } else {
+        end = mid - 1;
+      }
+    }
+    return ret;
+  }
+
+  bool unit_qulify(const vector<int> &l, int unit, int k) {
+    if (!unit)
+      return false;
+    auto ret = 0;
+    for (auto i : l) {
+      ret += i / unit;
+    }
+
+    return ret >= k;
+  }
+};

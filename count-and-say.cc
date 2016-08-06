d8add6b92970ee58db80d34fa63f4e1fa6fad1e0
rongyi <sqrongyi@163.com>
2016-08-06 10:06:33 +0800

    Add count and say template

---
+++ count-and-say.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/count-and-say
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param n the nth
+   * @return the nth sequence
+   */
+  string countAndSay(int n) {
+    // Write your code here
+  }
+};

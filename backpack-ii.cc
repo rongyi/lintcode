4fcd5f40e39fd54a1941076fc419a21e4d6ce1b0
rongyi <sqrongyi@163.com>
2016-08-06 15:48:04 +0800

    Add backpack II

---
+++ backpack-ii.cc
@@ -0,0 +1,35 @@
+// http://www.lintcode.com/zh-cn/problem/backpack-ii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param m: An integer m denotes the size of a backpack
+   * @param A & V: Given n items with size A[i] and value V[i]
+   * @return: The maximum value
+   */
+  int backPackII(int m, vector<int> A, vector<int> V) {
+    const int n = A.size();
+    if (m <= 0 || A.empty() || V.empty())
+      return 0;
+    vector<vector<int>> ret(n + 1, vector<int>(m + 1, 0));
+    ret[0][0] = 0;
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j <= m; j++) {
+        if (j - A[i - 1] < 0)
+          ret[i][j] = ret[i - 1][j];
+        else
+          ret[i][j] = std::max(ret[i - 1][j], ret[i - 1][j - A[i - 1]] + V[i - 1]);
+      }
+    }
+
+    return ret[n][m];
+  }
+};

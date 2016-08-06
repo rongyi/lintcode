2495fccb378d9eaac56fa42ae7a2cbff308893f9
rongyi <sqrongyi@163.com>
2016-08-06 11:22:45 +0800

    Modify count and say

--- count-and-say.cc
+++ count-and-say.cc
@@ -16,22 +16,21 @@ public:
    * @return the nth sequence
    */
   string countAndSay(int n) {
-    string seq = "1";
-
+    string ret = "1";
     for (int i = 1; i < n; i++) {
-      int len = seq.length();
+      const int len = ret.length();
       int start = 0;
-      string ret;
+      string tmp = "";
       for (int j = 1; j < len; j++) {
-        if (seq[j] != seq[j - 1]) {
-          ret += std::to_string(j - start) + seq[j - 1];
+        if (ret[j] != ret[j - 1]) {
+          tmp += std::to_string(j - start) + ret[j - 1];
           start = j;
         }
       }
-      ret += std::to_string(len - start) + seq[len - 1];
-      seq = ret;
+      tmp += std::to_string(len - start) + ret[len - 1];
+      ret = tmp;
     }
-    return seq;
+    return ret;
   }
 };
 

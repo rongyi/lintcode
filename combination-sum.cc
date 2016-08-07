75055cefe2ba500d2fa9160318776d2b6107ec59
rongyi <sqrongyi@163.com>
2016-08-07 11:41:27 +0800

    Add second function

--- combination-sum.cc
+++ combination-sum.cc
@@ -20,7 +20,8 @@ public:
     vector<int> cur_vec;
     std::sort(candidates.begin(), candidates.end());
     vector<vector<int>> ret;
-    dfs(ret, candidates, cur_vec, 0, target, 0);
+    // dfs(ret, candidates, cur_vec, 0, target, 0);
+    dfs2(ret, candidates, cur_vec, 0, target);
 
     // erase duplicate
     sort(ret.begin(), ret.end());
@@ -42,6 +43,19 @@ private:
       cur_vec.pop_back();
 
       dfs(ret, candidates, cur_vec, cur + 1, target, cur_sum);
+    }
+  }
+  void dfs2(vector<vector<int>> &ret, vector<int> &candidates, vector<int> &cur_vec, int cur_index, int target) {
+    if (target < 0)
+      return;
+    else if (target == 0) {
+      ret.push_back(cur_vec);
+    } else {
+      for (int i = cur_index; i < candidates.size(); i++) {
+        cur_vec.push_back(candidates[i]);
+        dfs2(ret, candidates, cur_vec, i, target - candidates[i]);
+        cur_vec.pop_back();
+      }
     }
   }
 };

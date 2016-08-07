eda9eaa9318e46f19e0762011288b0854435b1d5
rongyi <sqrongyi@163.com>
2016-08-07 10:17:50 +0800

    Graph is a tree

---
+++ graph-valid-tree.cc
@@ -0,0 +1,53 @@
+// http://www.lintcode.com/zh-cn/problem/graph-valid-tree
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param n an integer
+   * @param edges a list of undirected edges
+   * @return true if it's a valid tree, or false
+   */
+  bool validTree(int n, vector<vector<int>>& edges) {
+    if (n == 0 && edges.empty())
+      return true;
+    if (edges.empty())
+      return false;
+    vector<vector<int>> g(n, vector<int>());
+    vector<bool> visited(n, false);
+    for (auto &e : edges) {
+      g[e[0]].push_back(e[1]);
+      g[e[1]].push_back(e[0]);
+    }
+    if (!dfs(g, visited, 0, -1))
+      return false;
+    for (auto i : visited) {
+      // has island node
+      if (!i) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+private:
+  bool dfs(const vector<vector<int>> &g, vector<bool> &visited, int cur, int pre) {
+    if (visited[cur])
+      return false;
+    visited[cur] = true;
+    for (auto i : g[cur]) {
+      if (i != pre) {
+        if (!dfs(g, visited, i, cur))
+          return false;
+      }
+    }
+    return true;
+  }
+};

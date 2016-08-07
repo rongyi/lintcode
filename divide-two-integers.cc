9673153a4b743aa9c8bef768df9c963ce4da23f2
rongyi <sqrongyi@163.com>
2016-08-07 18:19:37 +0800

    Update local

--- divide-two-integers.cc
+++ divide-two-integers.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <limits>
 
 using std::vector;
 using std::cout;
@@ -16,6 +17,47 @@ public:
    * @return the result
    */
   int divide(int dividend, int divisor) {
-    // Write your code here
+    if (divisor == 0)
+      return -1;
+    if(dividend == std::numeric_limits<int>::min() && divisor==-1)
+      return std::numeric_limits<int>::max();
+
+    int sign = 1;
+
+    if ((dividend > 0) != (divisor > 0)) {
+      sign = -1;
+    }
+
+    long long ded = std::abs((long long)dividend);
+    long long der = std::abs((long long)divisor);
+    long long der_copy = der;
+    long long ret = 0;
+
+    // too slow!
+    // while (ded >= der) {
+    //   ded -= der;
+    //   if (std::numeric_limits<int>::max() - 1 == ret) {
+    //     return sign > 0 ? std::numeric_limits<int>::max() : std::numeric_limits<int>::min();
+    //   }
+    //   ret += 1;
+    // }
+
+    // make it fast
+    int step = 0;
+    while (der_copy < ded) {
+      der_copy <<= 1;
+      step++;
+    }
+
+    while (ded >= der) {
+      if (ded >= der_copy) {
+        ded -= der_copy;
+        ret += (long long)1 << step;
+      }
+      der_copy >>= 1;
+      step--;
+    }
+
+    return ret * sign;
   }
 };

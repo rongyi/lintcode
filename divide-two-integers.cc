e3b3fd83311ad3859d5acec55313a21dad6dd5de
rongyi <sqrongyi@163.com>
2016-08-07 17:32:10 +0800

    Add template

---
+++ divide-two-integers.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/divide-two-integers
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param dividend the dividend
+   * @param divisor the divisor
+   * @return the result
+   */
+  int divide(int dividend, int divisor) {
+    // Write your code here
+  }
+};

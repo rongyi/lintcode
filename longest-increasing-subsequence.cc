33559214a59ac9b2c965dd314e901a07a57490de
rongyi <sqrongyi@163.com>
2016-08-08 18:28:36 +0800

    Add longestIncreasingSubsequence

---
+++ longest-increasing-subsequence.cc
@@ -0,0 +1,22 @@
+// http://www.lintcode.com/zh-cn/problem/longest-increasing-subsequence
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+
+class Solution {
+public:
+  /**
+   * @param nums: The integer array
+   * @return: The length of LIS (longest increasing subsequence)
+   */
+  int longestIncreasingSubsequence(vector<int> nums) {
+    if (nums.empty())
+      return 0;
+  }
+};

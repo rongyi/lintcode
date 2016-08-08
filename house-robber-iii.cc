2c3d23f3166411ecc887e2b7525a864acb9f9a90
rongyi <sqrongyi@163.com>
2016-08-08 10:43:04 +0800

    Update houseRobber3

---
+++ house-robber-iii.cc
@@ -0,0 +1,54 @@
+// http://www.lintcode.com/zh-cn/problem/house-robber-iii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+  /**
+   * @param root: The root of binary tree.
+   * @return: The maximum amount of money you can rob tonight
+   */
+  int houseRobber3(TreeNode* root) {
+    if (!root)
+      return 0;
+    int rob_cur = 0;
+    int rob_cur_not = 0;
+
+    do_robber(root, &rob_cur, &rob_cur_not);
+
+    return std::max(rob_cur, rob_cur_not);
+  }
+private:
+  void do_robber(TreeNode *root, int *rob_cur, int * rob_cur_not) {
+    if (!root)
+      return;
+
+    int rob_cur_left = 0;
+    int rob_cur_left_not = 0;
+    int rob_cur_right = 0;
+    int rob_cur_right_not = 0;
+
+    do_robber(root->left, &rob_cur_left, &rob_cur_left_not);
+
+    do_robber(root->right, &rob_cur_right, &rob_cur_right_not);
+
+    *rob_cur = root->val + rob_cur_left_not + rob_cur_right_not;
+
+    // pick max
+    *rob_cur_not = std::max(rob_cur_left, rob_cur_left_not) + std::max(rob_cur_right, rob_cur_right_not);
+  }
+};

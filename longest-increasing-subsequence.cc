a03a70c663b0ef9f7a27b0a11d62ad366ebba27a
rongyi <sqrongyi@163.com>
2016-08-09 08:57:38 +0800

    Add longestIncreasingSubsequence

--- longest-increasing-subsequence.cc
+++ longest-increasing-subsequence.cc
@@ -18,5 +18,21 @@ public:
   int longestIncreasingSubsequence(vector<int> nums) {
     if (nums.empty())
       return 0;
+
+    vector<int> h_lst(nums.size(), 0);
+    int ret = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+      h_lst[i] = 1;
+      for (int j = 0; j < i; j++) {
+        if (nums[j] < nums[i]) {
+          h_lst[i] = std::max(h_lst[i], h_lst[j] + 1);
+        }
+      }
+      if (h_lst[i] > ret)
+        ret = h_lst[i];
+    }
+
+    return ret;
   }
 };

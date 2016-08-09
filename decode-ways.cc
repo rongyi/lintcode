83bf64f24ce9d0eed287a119ca176058d4dc8ce4
rongyi <sqrongyi@163.com>
2016-08-09 18:12:16 +0800

    Add decode num

--- decode-ways.cc
+++ decode-ways.cc
@@ -11,72 +11,32 @@ using std::string;
 
 class Solution {
 public:
-  Solution() {
-    for (int i = 0; i < 26; i++) {
-      secret_book_[i + 1] = 'A' + i;
-    }
-  }
   /**
    * @param s a string,  encoded message
    * @return an integer, the number of ways decoding
    */
   int numDecodings(string& s) {
-    vector<string> ret;
+    if(s.empty() || s[0]<'1' || s[0]>'9') return 0;
+    vector<int> dp(s.size()+1,0);
+    dp[0] = 1;
+    dp[1] = 1;
 
-    string cur;
-    bool is_invalid = false;
-    do_decode(s, ret, cur, is_invalid);
-
-
-    return is_invalid ? 0 : ret.size();
+    for(int i = 1; i < s.size(); i++) {
+      if(!isdigit(s[i]))
+        return 0;
+      int v = (s[i - 1] - '0') * 10 + (s[i] - '0');
+      /*
+        XY可以解码的条件是：9<XY<=26
+        Y可以单独解码的条件是：Y != '0'*/
+      if(v <= 26 && v > 9)
+        dp[i + 1] += dp[i - 1];
+      if(s[i] != '0')
+        dp[i + 1] += dp[i];
+      if(dp[i + 1] == 0)
+        return 0;
+    }
+    return dp[s.size()];
   }
-
-  void do_decode(string &s, vector<string> &ret, string &cur, bool &is_invalid) {
-    if (is_invalid)
-      return;
-    if (s.empty()) {
-      if (!cur.empty())
-        ret.push_back(cur);
-      return;
-    }
-
-    // 10 is a unit, can not decoded as 1 and 0
-    if (s.size() >= 2 && s.at(1) == '0') {
-      char c = secret_book_[std::stoi(s.substr(0, 2))];
-      cur += c;
-      std::string n = s.substr(2);
-      do_decode(n, ret, cur, is_invalid);
-      return;
-    }
-
-    int i = s.at(0) - '0';
-    // encount invalid sequence
-    if (i > 26 || i <= 0) {
-      is_invalid = true;
-      return;
-    }
-
-    char c = secret_book_[s.at(0) - '0'];
-    cur += c;
-    std::string n = s.substr(1);
-    do_decode(n, ret, cur, is_invalid);
-
-    if (s.size() >= 2) {
-      int i = (s.at(0) - '0') * 10 + (s.at(1) - '0');
-      // the last two will change to one
-      if (i >= 1 && i <= 26) {
-        cur.pop_back();
-        cur.pop_back();
-        char c = secret_book_[i];
-        cur += c;
-        std::string n = s.substr(2);
-        do_decode(n, ret, cur, is_invalid);
-      }
-    }
-  }
-
-private:
-  std::unordered_map<int, char> secret_book_;
 };
 
 int main()

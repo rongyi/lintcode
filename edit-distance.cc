7f6121fb48bfcc68fbefccbfbb657dec3a20c494
rongyi <sqrongyi@163.com>
2016-08-09 15:56:00 +0800

    Add edit distance

---
+++ edit-distance.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/edit-distance
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param word1 & word2: Two string.
+   * @return: The minimum number of steps.
+   */
+  int minDistance(string word1, string word2) {
+    // write your code here
+  }
+};

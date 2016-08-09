0dfef3bc1c534bc1567323b9dc41ab8f927ad791
rongyi <sqrongyi@163.com>
2016-08-09 11:13:53 +0800

    Add template

---
+++ digit-counts.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/digit-counts
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /*
+   * param k : As description.
+   * param n : As description.
+   * return: How many k's between 0 and n.
+   */
+  int digitCounts(int k, int n) {
+    // write your code here
+  }
+};

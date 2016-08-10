e50e08e374ef9ec99cdc97119fe0262e67468125
rongyi <sqrongyi@163.com>
2016-08-10 09:23:15 +0800

    Add interval sum

---
+++ interval-sum.cc
@@ -0,0 +1,55 @@
+// http://www.lintcode.com/zh-cn/problem/interval-sum
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+/**
+ * Definition of Interval:
+ * classs Interval {
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ */
+
+class Interval {
+public:
+  int start, end;
+  Interval(int start, int end) {
+    this->start = start;
+    this->end = end;
+  }
+};
+
+class Solution {
+public:
+  /**
+   *@param A, queries: Given an integer array and an query list
+   *@return: The result list
+   */
+  vector<long long> intervalSum(vector<int> &a, vector<Interval> &queries) {
+    vector<long long> ret;
+    if (a.empty())
+      return ret;
+    const int n = a.size();
+    vector<long long> prefix_sum(n + 1, 0);
+    for (int i = 0; i < n; i++) {
+      prefix_sum[i + 1] = prefix_sum[i] + a[i];
+    }
+
+    for (auto q : queries) {
+      int start = q.start;
+      int end = q.end;
+      long long cur_ret = prefix_sum[end + 1] - prefix_sum[start];
+      ret.push_back(cur_ret);
+    }
+
+    return ret;
+  }
+};

b0f5e58cc2cd2577156cf6bd7f4c01fcc9f9be5b
rongyi <sqrongyi@163.com>
2016-08-31 16:10:19 +0800

    Add partition array

---
+++ partition-array.cc
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  int partitionArray(vector<int> &nums, int k) {
+    // write your code here
+  }
+};

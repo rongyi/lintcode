c4f9195c2d3d599315d5a1a0a828d6f84b7bd5ca
rongyi <sqrongyi@163.com>
2016-09-13 15:23:08 +0800

    Update recursion

---
+++ print-numbers-by-recursion.cc
@@ -0,0 +1,44 @@
+// http://www.lintcode.com/zh-cn/problem/print-numbers-by-recursion
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param n: An integer.
+   * return : An array storing 1 to the largest number with n digits.
+   */
+  vector<int> numbersByRecursion(int n) {
+    vector<int> ret;
+    if (n <= 0) {
+      return ret;
+    } else if (n == 1) {
+      return {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    } else {
+      auto prev = numbersByRecursion(n - 1);
+      std::copy(prev.begin(), prev.end(), std::back_inserter(ret));
+      for (int i = 1; i <= 9; i++) {
+        ret.push_back(::pow(10, n - 1) * i);
+        for (auto p : prev) {
+          ret.push_back(::pow(10, n - 1) * i + p);
+        }
+      }
+      return ret;
+    }
+  }
+
+};
+
+int main()
+{
+  Solution so;
+  auto ret = so.numbersByRecursion(5);
+  return 0;
+}

15cbfb2b656c0d20efcacc6b4e54ce04c1905db3
rongyi <sqrongyi@163.com>
2016-09-13 16:47:21 +0800

    Add countOfSmallerNumber

---
+++ count-of-smaller-number.cc
@@ -0,0 +1,29 @@
+// http://www.lintcode.com/zh-cn/problem/count-of-smaller-number
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param A: An integer array
+   * @return: The number of element in the array that
+   *          are smaller that the given integer
+   */
+  vector<int> countOfSmallerNumber(vector<int> &A, vector<int> &queries) {
+    std::sort(A.begin(), A.end());
+    vector<int> ret;
+    for (auto q : queries) {
+      auto count = std::lower_bound(A.begin(), A.end(), q) - A.begin();
+      ret.push_back(count);
+    }
+
+    return ret;
+  }
+};

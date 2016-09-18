edc5a6521eea5d38f0cb6c6cc8e55799a5ff418c
rongyi <sqrongyi@163.com>
2016-09-18 11:58:59 +0800

    Add a easy solution to heat my brain
    Always be coding!

---
+++ clone-binary-tree.cc
@@ -0,0 +1,53 @@
+// http://www.lintcode.com/zh-cn/problem/clone-binary-tree
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+
+class TreeNode {
+public:
+    int val;
+    TreeNode *left, *right;
+    TreeNode(int val) {
+        this->val = val;
+        this->left = this->right = nullptr;
+    }
+};
+
+class Solution {
+public:
+  /**
+   * @param root: The root of binary tree
+   * @return root of new tree
+   */
+  TreeNode* cloneTree(TreeNode *root) {
+    if (!root)
+      return nullptr;
+    TreeNode *new_node = new TreeNode(root->val);
+    doClone(root->left, new_node, true);
+    doClone(root->right, new_node, false);
+
+    return new_node;
+  }
+
+  void doClone(TreeNode *root, TreeNode *prarent, bool isLeft) {
+    if (!root)
+      return;
+
+    TreeNode *new_node = new TreeNode(root->val);
+
+    if (isLeft) {
+      prarent->left = new_node;
+    } else {
+      prarent->right = new_node;
+    }
+
+    doClone(root->left, new_node, true);
+    doClone(root->right, new_node, false);
+  }
+};

b39e3c21f1f8bbf96afa754cfc3cbbbc186c91cb
rongyi <sqrongyi@163.com>
2016-09-27 08:46:18 +0800

    Add interval sum ii

---
+++ interval-sum-ii.cc
@@ -0,0 +1,37 @@
+// http://www.lintcode.com/zh-cn/problem/interval-sum-ii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /* you may need to use some attributes here */
+
+
+  /**
+   * @param A: An integer vector
+   */
+  Solution(vector<int> A) {
+    // write your code here
+  }
+
+  /**
+   * @param start, end: Indices
+   * @return: The sum from start to end
+   */
+  long long query(int start, int end) {
+    // write your code here
+  }
+
+  /**
+   * @param index, value: modify A[index] to value.
+   */
+  void modify(int index, int value) {
+    // write your code here
+  }
+};

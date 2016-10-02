c25ac89b01b8ef7d20af5e1c3b2c3a3a482569d3
rongyi <sqrongyi@163.com>
2016-10-02 10:10:19 +0800

    Add palindrome linked list

---
+++ palindrome-linked-list.cc
@@ -0,0 +1,26 @@
+// http://www.lintcode.com/zh-cn/problem/palindrome-linked-list
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+  /**
+   * @param head a ListNode
+   * @return a boolean
+   */
+  bool isPalindrome(ListNode* head) {
+    // Write your code here
+  }
+};

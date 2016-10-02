b4da8686a8f1cec2d810216bb1cd4cf15c13fabe
rongyi <sqrongyi@163.com>
2016-10-02 10:07:50 +0800

    Update minimum size subarray

--- minimum-size-subarray-sum.cc
+++ minimum-size-subarray-sum.cc
@@ -1,5 +1,6 @@
 // http://www.lintcode.com/zh-cn/problem/minimum-size-subarray-sum
 #include <vector>
+#include <algorithm>
 using std::vector;
 
 
@@ -11,6 +12,19 @@ public:
    * @return: an integer representing the minimum size of subarray
    */
   int minimumSize(vector<int> &nums, int s) {
+    int ret = std::numeric_limits<int>::max();
+    int sum = 0;
+    int left = 0;
+    for (int i = 0; i < nums.size(); i++) {
+      sum += nums[i];
+      if (sum >= s) {
+        while (left <= i && sum >= s) {
+          ret = std::min(ret, i - left + 1);
+          sum -= nums[left++];
+        }
+      }
+    }
 
+    return ret == std::numeric_limits<int>::max() ? -1 : ret;
   }
 };

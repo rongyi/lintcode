57805eaa6e89ca5f03e4fe6e0c38315933ef9eb9
rongyi <sqrongyi@163.com>
2016-10-02 09:48:36 +0800

    Add minimum size subarray

---
+++ minimum-size-subarray-sum.cc
@@ -0,0 +1,16 @@
+// http://www.lintcode.com/zh-cn/problem/minimum-size-subarray-sum
+#include <vector>
+using std::vector;
+
+
+class Solution {
+public:
+  /**
+   * @param nums: a vector of integers
+   * @param s: an integer
+   * @return: an integer representing the minimum size of subarray
+   */
+  int minimumSize(vector<int> &nums, int s) {
+
+  }
+};

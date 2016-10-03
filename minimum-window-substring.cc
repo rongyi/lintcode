ca35e8c7012980bfcca2cd37f3c4644904feff17
rongyi <sqrongyi@163.com>
2016-10-03 08:04:25 +0800

    Add minimum window substring

---
+++ minimum-window-substring.cc
@@ -0,0 +1,16 @@
+// http://www.lintcode.com/zh-cn/problem/minimum-window-substring
+#include <string>
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param source: A string
+   * @param target: A string
+   * @return: A string denote the minimum window
+   *          Return "" if there is no such a string
+   */
+  string minWindow(string &source, string &target) {
+    // write your code here
+  }
+};

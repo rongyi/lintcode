e147b6fcb1a846f5c4cad4ddad8f3e8f4fa4032d
rongyi <sqrongyi@163.com>
2016-10-03 08:53:40 +0800

    Update minimum window substring

--- minimum-window-substring.cc
+++ minimum-window-substring.cc
@@ -1,6 +1,10 @@
 // http://www.lintcode.com/zh-cn/problem/minimum-window-substring
 #include <string>
+#include <unordered_map>
+#include <algorithm>
+
 using std::string;
+using std::unordered_map;
 
 class Solution {
 public:
@@ -11,6 +15,39 @@ public:
    *          Return "" if there is no such a string
    */
   string minWindow(string &source, string &target) {
-    // write your code here
+    unordered_map<char, int> mark;
+    unordered_map<char, int> check;
+    for (auto t : target)
+      ++mark[t];
+    int begin = 0;
+    int end = 0;
+    const int n = source.length();
+    int min = std::numeric_limits<int>::max();
+    string ret;
+
+    for (; end < n; ++end) {
+      if (mark.count(source[end]) != 0)
+        ++check[source[end]];
+      while (every(check, mark)) {
+        const int cur_len = end - begin + 1;
+        if (cur_len < min) {
+          min = cur_len;
+          ret = source.substr(begin, cur_len);
+        }
+        --check[source[begin]];
+        ++begin;
+      }
+    }
+    return min == std::numeric_limits<int>::max() ? "" : ret;
+  }
+
+private:
+  bool every(unordered_map<char, int> &check, unordered_map<char, int> &mark) {
+    for (auto m : mark) {
+      if (check[m.first] < m.second)
+        return false;
+    }
+
+    return true;
   }
 };

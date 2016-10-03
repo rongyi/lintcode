86e62a1182f94c21a7ceec3ad76e730ba5a83001
rongyi <sqrongyi@163.com>
2016-10-03 09:17:09 +0800

    Update segment tree modify
    This is a bugged version

--- segment-tree-modify.cc
+++ segment-tree-modify.cc
@@ -34,6 +34,17 @@ public:
    *@return: void
    */
   void modify(SegmentTreeNode *root, int index, int value) {
-    // write your code here
+    if (root->start == root->end && index == root->start) {
+      root->max = value;
+    }
+    const int mid = root->start + (root->end - root->start) / 2;
+    if (value > root->max)
+      root->max = value;
+
+    if (index <= mid) {
+      modify(root->left, index, value);
+    } else {
+      modify(root->right, index, value);
+    }
   }
 };

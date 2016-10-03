eb61d7047f440423a01ffb7c5df22c31142126be
rongyi <sqrongyi@163.com>
2016-10-03 09:24:17 +0800

    Update segment tree

--- segment-tree-modify.cc
+++ segment-tree-modify.cc
@@ -1,4 +1,5 @@
 // http://www.lintcode.com/zh-cn/problem/segment-tree-modify
+#include <algorithm>
 /**
  * Definition of SegmentTreeNode:
  * class SegmentTreeNode {
@@ -34,17 +35,20 @@ public:
    *@return: void
    */
   void modify(SegmentTreeNode *root, int index, int value) {
+    if (!root)
+      return;
     if (root->start == root->end && index == root->start) {
       root->max = value;
+      return;
     }
     const int mid = root->start + (root->end - root->start) / 2;
-    if (value > root->max)
-      root->max = value;
+
 
     if (index <= mid) {
       modify(root->left, index, value);
     } else {
       modify(root->right, index, value);
     }
+    root->max = std::max(root->left->max, root->right->max);
   }
 };

a6b1f0501e0e469ff66c67563d71b6945f351d05
rongyi <sqrongyi@163.com>
2016-10-06 21:04:37 +0800

    Update merge intervals

--- merge-intervals.cc
+++ merge-intervals.cc
@@ -1,5 +1,6 @@
 // http://www.lintcode.com/zh-cn/problem/merge-intervals
 #include <vector>
+#include <algorithm>
 using std::vector;
 /**
  * Definition of Interval:
@@ -11,12 +12,20 @@ using std::vector;
  *     }
  */
 class Interval {
+public:
   int start, end;
   Interval(int start, int end) {
     this->start = start;
     this->end = end;
   }
 };
+
+bool cmp(const Interval &a, const Interval &b) {
+  if (a.start == b.start)
+    return a.end < b.end;
+  return a.start < b.start;
+}
+
 class Solution {
 public:
   /**
@@ -24,6 +33,18 @@ public:
    * @return: A new interval list.
    */
   vector<Interval> merge(vector<Interval> &intervals) {
-    // write your code here
+    if (intervals.empty())
+      return intervals;
+    std::sort(intervals.begin(), intervals.end(), cmp);
+    for (int i = 1; i < intervals.size(); i++) {
+      if (intervals[i - 1].end >= intervals[i].start) {
+        if (intervals[i - 1].end < intervals[i].end) {
+          intervals[i - 1].end = intervals[i].end;
+        }
+        intervals.erase(intervals.begin() + i);
+        --i;
+      }
+    }
+    return intervals;
   }
 };

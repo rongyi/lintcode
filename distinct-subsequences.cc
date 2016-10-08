549d0bea047ec59ff5b166ab93cdf82d2634e6a3
rongyi <sqrongyi@163.com>
2016-10-08 11:08:17 +0800

    Update distinct subsequences

--- distinct-subsequences.cc
+++ distinct-subsequences.cc
@@ -1,6 +1,8 @@
 // http://www.lintcode.com/zh-cn/problem/distinct-subsequences
 #include <string>
+#include <vector>
 using std::string;
+using std::vector;
 
 class Solution {
 public:
@@ -9,6 +11,21 @@ public:
    * @return: Count the number of distinct subsequences
    */
   int numDistinct(string &S, string &T) {
+    const int n = S.size();
+    const int m = T.size();
+    vector<int> dp(n + 1, 1);
 
+    for (int i = 1; i <= m; i++) {
+      int up_left = dp[0];
+      dp[0] = 0;
+      for (int j = 1; j <= n; j++) {
+        int tmp = dp[j];
+        dp[j] = dp[j - 1];
+        if (S[j - 1] == T[i - 1])
+          dp[j] += up_left;
+        up_left = tmp;
+      }
+    }
+    return dp[n];
   }
 };

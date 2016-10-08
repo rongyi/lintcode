c631de56705e9c5ac9b9e78471776b067c1e4f30
rongyi <sqrongyi@163.com>
2016-10-08 16:20:55 +0800

    Add majorityNumberIII template

---
+++ majority-number-iii.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/majority-number-iii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param nums: A list of integers
+   * @param k: As described
+   * @return: The majority number
+   */
+  int majorityNumber(vector<int> nums, int k) {
+
+  }
+};

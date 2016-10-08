104f65644b75dda9c14ceead5d1d70ebeed5356e
rongyi <sqrongyi@163.com>
2016-10-08 10:56:53 +0800

    Add distinct subsequences template

---
+++ distinct-subsequences.cc
@@ -0,0 +1,14 @@
+// http://www.lintcode.com/zh-cn/problem/distinct-subsequences
+#include <string>
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param S, T: Two string.
+   * @return: Count the number of distinct subsequences
+   */
+  int numDistinct(string &S, string &T) {
+
+  }
+};

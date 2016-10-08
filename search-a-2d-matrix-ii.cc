d814f2ca538e1d8d871bc253d5fb38dadee179b3
rongyi <sqrongyi@163.com>
2016-10-08 15:21:24 +0800

    Update searchMatrix

--- search-a-2d-matrix-ii.cc
+++ search-a-2d-matrix-ii.cc
@@ -15,7 +15,23 @@ public:
    * @param target: An integer you want to search in matrix
    * @return: An integer indicate the total occurrence of target in the given matrix
    */
-  int searchMatrix(vector<vector<int> > &matrix, int target) {
-    // write your code here
+  int searchMatrix(vector<vector<int>> &matrix, int target) {
+    if (matrix.empty())
+      return 0;
+    const int m = matrix.size();
+    const int n = matrix[0].size();
+
+    int ret = 0;
+    for (int i = m - 1, j = 0; i >= 0 && j < n;) {
+      if (matrix[i][j] == target) {
+        ret++;
+      }
+      if (matrix[i][j] < target) {
+        ++j;
+      } else {
+        --i;
+      }
+    }
+    return ret;
   }
 };

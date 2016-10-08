b0d1c76a6a72079a34542f51fc28d46fcd62220b
rongyi <sqrongyi@163.com>
2016-10-08 14:06:18 +0800

    Add minimum adjustment template

---
+++ minimum-adjustment-cost.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/minimum-adjustment-cost
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param A: An integer array.
+   * @param target: An integer.
+   */
+  int MinAdjustmentCost(vector<int> A, int target) {
+    // write your code here
+  }
+};

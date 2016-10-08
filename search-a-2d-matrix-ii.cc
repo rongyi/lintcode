4b394ad25111dc58800e4dc5c49522ef52cb3c01
rongyi <sqrongyi@163.com>
2016-10-08 15:05:54 +0800

    Add searchMatrix template

---
+++ search-a-2d-matrix-ii.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/search-a-2d-matrix-ii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param matrix: A list of lists of integers
+   * @param target: An integer you want to search in matrix
+   * @return: An integer indicate the total occurrence of target in the given matrix
+   */
+  int searchMatrix(vector<vector<int> > &matrix, int target) {
+    // write your code here
+  }
+};

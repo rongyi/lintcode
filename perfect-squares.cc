dbf1e4bd9dcbf6e3afc0bd452cc717fe382e7902
rongyi <sqrongyi@163.com>
2016-10-09 16:58:25 +0800

    Add perfect squares template

---
+++ perfect-squares.cc
@@ -0,0 +1,11 @@
+// http://www.lintcode.com/zh-cn/problem/perfect-squares
+class Solution {
+public:
+  /**
+   * @param n a positive integer
+   * @return an integer
+   */
+  int numSquares(int n) {
+    // Write your code here
+  }
+};

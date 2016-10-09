1c03b760d26aa4aa906f5db0c556c8773f00a0d3
rongyi <sqrongyi@163.com>
2016-10-09 16:44:32 +0800

    Add removeDuplicatesII template

---
+++ remove-duplicates-from-sorted-array-ii.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/remove-duplicates-from-sorted-array-ii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param A: a list of integers
+   * @return : return an integer
+   */
+  int removeDuplicates(vector<int> &nums) {
+
+  }
+};

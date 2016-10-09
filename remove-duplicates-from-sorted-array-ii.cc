b9e6b6b733629e96b421d13245506b077f33d060
rongyi <sqrongyi@163.com>
2016-10-09 16:54:10 +0800

    Update removeDuplicates II

--- remove-duplicates-from-sorted-array-ii.cc
+++ remove-duplicates-from-sorted-array-ii.cc
@@ -15,6 +15,24 @@ public:
    * @return : return an integer
    */
   int removeDuplicates(vector<int> &nums) {
+    if (nums.size() <= 2)
+      return nums.size();
 
+    int index = 2;
+    for (int i = 2; i < nums.size(); i++) {
+      if (nums[i] != nums[index - 2])
+        nums[index++] = nums[i];
+    }
+
+    return index;
   }
 };
+
+int main()
+{
+  Solution so;
+  vector<int> test{1, 2, 2, 3, 3, 3};
+  auto ret = so.removeDuplicates(test);
+  cout << ret << endl;
+  return 0;
+}

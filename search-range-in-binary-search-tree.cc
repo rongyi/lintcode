f1a41e15d5cc35bbf920548c7d5a261ad00d5131
rongyi <sqrongyi@163.com>
2016-10-10 15:58:04 +0800

    Add search range in binary search tree

---
+++ search-range-in-binary-search-tree.cc
@@ -0,0 +1,33 @@
+// http://www.lintcode.com/zh-cn/problem/search-range-in-binary-search-tree
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+class Solution {
+public:
+  /**
+   * @param root: The root of the binary search tree.
+   * @param k1 and k2: range k1 to k2.
+   * @return: Return all keys that k1<=key<=k2 in ascending order.
+   */
+  vector<int> searchRange(TreeNode* root, int k1, int k2) {
+    // write your code here
+  }
+};

40777b2041e6b7f9a4457d97325f213b06fe9495
rongyi <sqrongyi@163.com>
2016-10-10 11:33:58 +0800

    Add template

---
+++ maximum-subarray-iii.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/maximum-subarray-iii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param nums: A list of integers
+   * @param k: An integer denote to find k non-overlapping subarrays
+   * @return: An integer denote the sum of max k non-overlapping subarrays
+   */
+  int maxSubArray(vector<int> nums, int k) {
+
+  }
+};

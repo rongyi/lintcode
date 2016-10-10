2212c630e4fc238693030f12df48c03879898793
rongyi <sqrongyi@163.com>
2016-10-10 16:34:14 +0800

    Add ksumII

---
+++ k-sum-ii.cc
@@ -0,0 +1,22 @@
+// http://www.lintcode.com/zh-cn/problem/k-sum-ii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param A: an integer array.
+   * @param k: a positive integer (k <= length(A))
+   * @param target: a integer
+   * @return a list of lists of integer
+   */
+  vector<vector<int> > kSumII(vector<int> A, int k, int target) {
+    // write your code here
+  }
+};

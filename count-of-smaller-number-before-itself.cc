fcbf1ba4595c5c47ba6e398c7105628a6c827c0f
rongyi <sqrongyi@163.com>
2016-10-10 08:45:08 +0800

    Add countOfSmallerNumber before itself

---
+++ count-of-smaller-number-before-itself.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/count-of-smaller-number-before-itself
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param A: An integer array
+   * @return: Count the number of element before this element 'ai' is
+   *          smaller than it and return count number array
+   */
+  vector<int> countOfSmallerNumberII(vector<int> &A) {
+
+  }
+};

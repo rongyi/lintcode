304f02842b44f1672f3b66ff95a7e786f10e764b
rongyi <sqrongyi@163.com>
2016-10-10 11:38:08 +0800

    Update maximum subarray

--- maximum-subarray-iii.cc
+++ maximum-subarray-iii.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using std::vector;
 using std::cout;
@@ -16,6 +17,22 @@ public:
    * @return: An integer denote the sum of max k non-overlapping subarrays
    */
   int maxSubArray(vector<int> nums, int k) {
+    const int n = nums.size();
+    if (n < k)
+      return 0;
+    vector<vector<int>> global_max(n + 1, vector<int>(k + 1, 0));
+    for (int j = 1; j <= k; j++) {
+      int local_max = std::numeric_limits<int>::min();
+      for (int i = j - 1; i < n; i++) {
+        local_max = std::max(local_max, global_max[i][j - 1]) + nums[i];
 
+        if (i == j - 1) {
+          global_max[i + 1][j] = local_max;
+        } else {
+          global_max[i + 1][j] = std::max(global_max[i][j], local_max);
+        }
+      }
+    }
+    return global_max[n][k];
   }
 };

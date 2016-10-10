e26605f3ca90d2d80e94600e95d2c763fb0d9325
rongyi <sqrongyi@163.com>
2016-10-10 18:21:08 +0800

    Update construct tree

--- construct-binary-tree-from-inorder-and-postorder-traversal.cc
+++ construct-binary-tree-from-inorder-and-postorder-traversal.cc
@@ -39,6 +39,29 @@ class Solution {
    */
 public:
   TreeNode *buildTree(vector<int> &inorder, vector<int> &postorder) {
-    // write your code here
+    if (inorder.empty() || inorder.size() != postorder.size())
+      return nullptr;
+    TreeNode *ret = nullptr;
+
+    doBuild(ret, postorder, inorder, postorder.size() - 1, 0, postorder.size());
+
+    return ret;
+  }
+private:
+  void doBuild(TreeNode * &root, vector<int> &postorder, vector<int> &inorder,
+               int post_index, int in_index, int in_size) {
+    if (in_index >= in_size)
+      return;
+    root = new TreeNode(postorder[post_index]);
+    int cur_in_size;
+    for (int i = in_index; i < in_size; i++) {
+      if (inorder[i] == postorder[post_index]) {
+        cur_in_size = i;
+        break;
+      }
+    }
+
+    doBuild(root->left, postorder, inorder, post_index - (in_size - cur_in_size), in_index, cur_in_size);
+    doBuild(root->right, postorder, inorder, post_index - 1, cur_in_size + 1, in_size);
   }
 };

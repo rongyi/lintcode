916b80877fb5a23135410e54549b91c0f78c9870
rongyi <sqrongyi@163.com>
2016-10-10 10:21:29 +0800

    Add k sum

---
+++ k-sum.cc
@@ -0,0 +1,18 @@
+// http://www.lintcode.com/zh-cn/problem/k-sum
+#include <vector>
+#include <iostream>
+
+using std::vector;
+
+class Solution {
+public:
+  /**
+   * @param A: an integer array.
+   * @param k: a positive integer (k <= length(A))
+   * @param target: a integer
+   * @return an integer
+   */
+  int kSum(vector<int> A, int k, int target) {
+
+  }
+};

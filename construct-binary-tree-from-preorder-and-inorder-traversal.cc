f4d1992e748d2752ae59d23d328cdaedb041c175
rongyi <sqrongyi@163.com>
2016-10-10 17:11:01 +0800

    Update contruction tree

--- construct-binary-tree-from-preorder-and-inorder-traversal.cc
+++ construct-binary-tree-from-preorder-and-inorder-traversal.cc
@@ -27,7 +27,7 @@ public:
   TreeNode *left, *right;
   TreeNode(int val) {
     this->val = val;
-    this->left = this->right = NULL;
+    this->left = this->right = nullptr;
   }
 };
 
@@ -39,6 +39,34 @@ class Solution {
    */
 public:
   TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
-    // write your code here
+    if (preorder.empty() || preorder.size() != inorder.size())
+      return nullptr;
+
+    TreeNode *ret = nullptr;
+
+    doBuild(ret, preorder, inorder,
+            0, 0, inorder.size());
+
+    return ret;
+  }
+private:
+  void doBuild(TreeNode * &root, vector<int> &preorder, vector<int> &inorder,
+               int pre_index,
+               int in_index,
+               int in_size) {
+    if (in_index >= in_size)
+      return;
+    root = new TreeNode(preorder[pre_index]);
+    int cur_in_size;
+    for (int i = in_index; i < in_size; i++) {
+      if (inorder[i] == preorder[pre_index]) {
+        cur_in_size = i;
+        break;
+      }
+    }
+    doBuild(root->left, preorder, inorder,
+            pre_index + 1, in_index, cur_in_size);
+    doBuild(root->right, preorder, inorder,
+            pre_index + cur_in_size - in_index + 1, cur_in_size + 1, in_size);
   }
 };

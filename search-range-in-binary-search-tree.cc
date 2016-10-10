e9582d150bd8cea007d86fd03ff4a000b76f1be7
rongyi <sqrongyi@163.com>
2016-10-10 15:58:47 +0800

    Update search range in binary search tree

--- search-range-in-binary-search-tree.cc
+++ search-range-in-binary-search-tree.cc
@@ -20,6 +20,17 @@ using std::string;
  *     }
  * }
  */
+
+class TreeNode {
+public:
+  int val;
+  TreeNode *left, *right;
+  TreeNode(int val) {
+    this->val = val;
+    this->left = this->right = NULL;
+  }
+};
+
 class Solution {
 public:
   /**
@@ -28,6 +39,6 @@ public:
    * @return: Return all keys that k1<=key<=k2 in ascending order.
    */
   vector<int> searchRange(TreeNode* root, int k1, int k2) {
-    // write your code here
+
   }
 };

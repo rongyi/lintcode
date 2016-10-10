bddf82f03e4efcf3a8a4745d16d12bf9407be337
rongyi <sqrongyi@163.com>
2016-10-10 16:39:15 +0800

    Update k sum ii

--- k-sum-ii.cc
+++ k-sum-ii.cc
@@ -17,6 +17,25 @@ public:
    * @return a list of lists of integer
    */
   vector<vector<int> > kSumII(vector<int> A, int k, int target) {
-    // write your code here
+    vector<vector<int>> ret;
+    vector<int> cur;
+
+    dosum(ret, cur, A, k, target, 0);
+
+    return ret;
+  }
+private:
+  void dosum(vector<vector<int>> &ret, vector<int> path, vector<int> nodes, int k, int remain, int index) {
+    if (path.size() == k) {
+      if (remain == 0) {
+        ret.push_back(path);
+      }
+      return;
+    }
+    for (int i = index; i < nodes.size(); i++) {
+      path.push_back(nodes[i]);
+      dosum(ret, path, nodes, k, remain - nodes[i], i + 1);
+      path.pop_back();
+    }
   }
 };

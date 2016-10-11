70a88b7568195e898b5f3c5b36816d1773817cea
rongyi <sqrongyi@163.com>
2016-10-11 14:21:39 +0800

    Add word search II

---
+++ word-search-ii.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/word-search-ii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param board: A list of lists of character
+   * @param words: A list of string
+   * @return: A list of string
+   */
+  vector<string> wordSearchII(vector<vector<char> > &board, vector<string> &words) {
+    // write your code here
+  }
+};

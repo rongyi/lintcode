9d8a0e182eab63824e415a0c37d74925ce87b636
rongyi <sqrongyi@163.com>
2016-10-11 10:19:01 +0800

    Add minCut

---
+++ palindrome-partitioning-ii.cc
@@ -0,0 +1,11 @@
+// http://www.lintcode.com/zh-cn/problem/palindrome-partitioning-ii
+class Solution {
+public:
+  /**
+   * @param s a string
+   * @return an integer
+   */
+  int minCut(string s) {
+    // write your code here
+  }
+};

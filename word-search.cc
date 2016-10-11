fe92a4fa8e8da7fc2b63bd092ad1dd6a1900dc64
rongyi <sqrongyi@163.com>
2016-10-11 13:43:04 +0800

    Add word search

---
+++ word-search.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/word-search
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param board: A list of lists of character
+   * @param word: A string
+   * @return: A boolean
+   */
+  bool exist(vector<vector<char> > &board, string word) {
+    // write your code here
+  }
+};

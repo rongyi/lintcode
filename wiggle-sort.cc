a22fb06cdcc9f7f6c362204d741232b36664b1bb
rongyi <sqrongyi@163.com>
2016-10-12 17:02:03 +0800

    Update wiggleSort

---
+++ wiggle-sort.cc
@@ -0,0 +1,39 @@
+// http://www.lintcode.com/zh-cn/problem/wiggle-sort
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param nums a list of integer
+   * @return void
+   */
+  void wiggleSort(vector<int>& nums) {
+    vector<int> tmp = nums;
+    std::sort(tmp.begin(), tmp.end());
+    const int n = nums.size();
+    int k = (n + 1) / 2;
+    int j = n;
+    for (int i = 0; i < n; i++) {
+      nums[i] = i & 1 ? tmp[--j] : tmp[--k];
+    }
+  }
+};
+
+int main()
+{
+  Solution so;
+  vector<int> test{3, 5, 2, 1, 6, 4};
+  so.wiggleSort(test);
+  for (auto i : test)
+    cout << i << " ";
+  cout << endl;
+  return 0;
+}

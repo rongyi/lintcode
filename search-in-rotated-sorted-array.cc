3abbe16d5cf26ea7a9bf64e37e23080f9c505077
rongyi <sqrongyi@163.com>
2016-10-12 10:49:42 +0800

    Update search in rotated array

--- search-in-rotated-sorted-array.cc
+++ search-in-rotated-sorted-array.cc
@@ -16,6 +16,25 @@ class Solution {
    */
 public:
   int search(vector<int> &A, int target) {
+    int first = 0;
+    int last = A.size();
 
+    while (first != last) {
+      const int mid = first + (last - first) / 2;
+      if (A[mid] == target)
+        return mid;
+      if (A[first] <= A[mid]) {
+        if (A[first] <= target && target < A[mid])
+          last = mid;
+        else
+          first = mid + 1;
+      } else {
+        if (A[mid] < target && target <= A[last - 1])
+          first = mid + 1;
+        else
+          last = mid;
+      }
+    }
+    return -1;
   }
 };

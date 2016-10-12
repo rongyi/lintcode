4e16604156d9772202cb47a8cdd6914d281b7c23
rongyi <sqrongyi@163.com>
2016-10-12 10:43:43 +0800

    Add search in rotated array

---
+++ search-in-rotated-sorted-array.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/search-in-rotated-sorted-array
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+  /**
+   * param A : an integer ratated sorted array
+   * param target :  an integer to be searched
+   * return : an integer
+   */
+public:
+  int search(vector<int> &A, int target) {
+
+  }
+};

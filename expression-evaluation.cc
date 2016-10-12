eb20344f04965ed919921ec9091108de4e1e5eda
rongyi <sqrongyi@163.com>
2016-10-12 09:43:51 +0800

    Add expression evaluation

---
+++ expression-evaluation.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/expression-evaluation
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param expression: a vector of strings;
+   * @return: an integer
+   */
+  int evaluateExpression(vector<string> &expression) {
+    // write your code here
+  }
+};

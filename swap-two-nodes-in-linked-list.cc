e584ba1d54c3db4ef98b9c8149470fca9b0aec46
rongyi <sqrongyi@163.com>
2016-10-12 15:28:28 +0800

    Add linked list swap

---
+++ swap-two-nodes-in-linked-list.cc
@@ -0,0 +1,27 @@
+// http://www.lintcode.com/zh-cn/problem/swap-two-nodes-in-linked-list
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+  /**
+   * @param head a ListNode
+   * @oaram v1 an integer
+   * @param v2 an integer
+   * @return a new head of singly-linked list
+   */
+  ListNode* swapNodes(ListNode* head, int v1, int v2) {
+
+  }
+};

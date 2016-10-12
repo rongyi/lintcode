bb90c0764580040bb13427cf486cda2d7f5b41cc
rongyi <sqrongyi@163.com>
2016-10-12 11:27:54 +0800

    Update jump game ii

--- jump-game-ii.cc
+++ jump-game-ii.cc
@@ -11,21 +11,30 @@ using std::string;
 class Solution {
 public:
   /**
-   * @param A: A list of lists of integers
+   * @param a: a list of lists of integers
    * @return: An integer
    */
-  int jump(vector<int> A) {
+  int jump(vector<int> a) {
     int ret = 0;
     int last = 0;
     int cur = 0;
-    for (int i = 0; i < A.size(); i++) {
+    for (int i = 0; i < a.size(); i++) {
       if (i > last) {
         last = cur;
         ++ret;
       }
-      cur = std::max(cur, i + A[i]);
+      cur = std::max(cur, i + a[i]);
     }
 
     return ret;
   }
 };
+
+int main()
+{
+  vector<int> input{2, 3, 1, 1, 4};
+  Solution so;
+  auto ret = so.jump(input);
+  cout << ret << endl;
+  return 0;
+}

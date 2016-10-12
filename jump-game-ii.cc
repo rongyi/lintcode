b47400f17ba39397a04992d061f8d49f19f2d58b
rongyi <sqrongyi@163.com>
2016-10-12 11:20:51 +0800

    Add jump game ii

---
+++ jump-game-ii.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/jump-game-ii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param A: A list of lists of integers
+   * @return: An integer
+   */
+  int jump(vector<int> A) {
+
+  }
+};

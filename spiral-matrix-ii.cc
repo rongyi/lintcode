b5fcf6ddc818be0ae66772f9df5e72b364be7f50
rongyi <sqrongyi@163.com>
2016-10-13 09:52:19 +0800

    Add mk spiral matrix

---
+++ spiral-matrix-ii.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/spiral-matrix-ii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param n an integer
+   * @return a square matrix
+   */
+  vector<vector<int>> generateMatrix(int n) {
+
+  }
+};

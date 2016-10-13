26baf465cadbaa50f618df6508167748060c72e8
rongyi <sqrongyi@163.com>
2016-10-13 09:03:19 +0800

    Add spiralOrder

---
+++ spiral-matrix.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/spiral-matrix
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param matrix a matrix of m x n elements
+   * @return an integer array
+   */
+  vector<int> spiralOrder(vector<vector<int>>& matrix) {
+
+  }
+};

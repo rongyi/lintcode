ef794c4fe2dcb1c59b18dde1c2f006e3b18eed1a
rongyi <sqrongyi@163.com>
2016-10-13 10:54:57 +0800

    Add submatrixSum zero

---
+++ submatrix-sum.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/submatrix-sum
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param matrix an integer matrix
+   * @return the coordinate of the left-up and right-down number
+   */
+  vector<vector<int>> submatrixSum(vector<vector<int>>& matrix) {
+
+  }
+};

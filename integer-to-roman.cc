c9a7bed47e8611e5cef549637f6639e78a417547
rongyi <sqrongyi@163.com>
2016-10-17 10:14:16 +0800

    Add integer to roman

--- integer-to-roman.cc
+++ integer-to-roman.cc
@@ -15,6 +15,18 @@ public:
    * @return Roman representation
    */
   string intToRoman(int n) {
-    // Write your code here
+    string ret;
+    static const int radix[] = {1000, 900, 500, 400, 100, 90,
+                                50, 40, 10, 9, 5, 4, 1};
+    static const string symbol[] = {"M","CM", "D", "CD", "C",
+                                    "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    for (int i = 0; n > 0; i++) {
+      int count = n / radix[i];
+      n %= radix[i];
+      for (; count > 0; --count)
+        ret += symbol[i];
+    }
+
+    return ret;
   }
 };

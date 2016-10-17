5b35e31dfc6a8d6d84e6e31fec0d3e6723037b56
rongyi <sqrongyi@163.com>
2016-10-17 09:33:44 +0800

    Add coins in a line

---
+++ coins-in-a-line-ii.cc
@@ -0,0 +1,11 @@
+// http://www.lintcode.com/zh-cn/problem/coins-in-a-line-ii
+class Solution {
+public:
+  /**
+   * @param values: a vector of integers
+   * @return: a boolean which equals to true if the first player will win
+   */
+  bool firstWillWin(vector<int> &values) {
+
+  }
+};

2ff7cb77203cc407866da4878832221e7b82af6b
rongyi <sqrongyi@163.com>
2016-10-17 10:09:17 +0800

    Add integer to roman

---
+++ integer-to-roman.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/integer-to-roman
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param n The integer
+   * @return Roman representation
+   */
+  string intToRoman(int n) {
+    // Write your code here
+  }
+};

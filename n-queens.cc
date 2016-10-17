3c563c7e24f25d2054d5867fa1bde62fd9e38606
rongyi <sqrongyi@163.com>
2016-10-17 10:41:12 +0800

    Add n-queen

---
+++ n-queens.cc
@@ -0,0 +1,22 @@
+// http://www.lintcode.com/zh-cn/problem/n-queens
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * Get all distinct N-Queen solutions
+   * @param n: The number of queens
+   * @return: All distinct solutions
+   * For example, A string '...Q' shows a queen on forth position
+   */
+  vector<vector<string> > solveNQueens(int n) {
+
+  }
+};

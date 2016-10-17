f1811f3f13adfa3fb0cd2c606325266a132de687
rongyi <sqrongyi@163.com>
2016-10-17 17:37:41 +0800

    Add median of two sorted array

---
+++ median-of-two-sorted-arrays.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/median-of-two-sorted-arrays
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param A: An integer array.
+   * @param B: An integer array.
+   * @return: a double whose format is *.5 or *.0
+   */
+  double findMedianSortedArrays(vector<int> A, vector<int> B) {
+
+  }
+};

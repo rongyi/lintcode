134228bdf199de304f03ade2cfa7013abd1f999c
rongyi <sqrongyi@163.com>
2016-10-17 10:22:06 +0800

    Update roman to integer

--- roman-to-integer.cc
+++ roman-to-integer.cc
@@ -15,6 +15,28 @@ public:
    * @return an integer
    */
   int romanToInt(string& s) {
-    // Write your code here
+    auto map = [](const char c) -> int {
+      switch (c) {
+      case 'I': return 1;
+      case 'V': return 5;
+      case 'X': return 10;
+      case 'L': return 50;
+      case 'C': return 100;
+      case 'D': return 500;
+      case 'M': return 1000;
+      default: return 0;
+      }
+    };
+    int ret = 0;
+
+    for (int i = 0; i < s.size(); i++) {
+      if (i > 0 && map(s[i]) > map(s[i - 1])) {
+        ret += (map(s[i]) - 2 * map(s[i - 1]));
+      } else {
+        ret += map(s[i]);
+      }
+    }
+
+    return ret;
   }
 };

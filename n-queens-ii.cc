f756d6fbb233e5af4e8943dd5add0136766ee797
rongyi <sqrongyi@163.com>
2016-10-17 11:02:56 +0800

    Update n-queens

---
+++ n-queens-ii.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/n-queens-ii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * Calculate the total number of distinct N-Queen solutions.
+   * @param n: The number of queens.
+   * @return: The total number of distinct solutions.
+   */
+  int totalNQueens(int n) {
+    // write your code here
+  }
+};

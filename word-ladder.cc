41c5462253cac75f31a527ed6447967253550f5b
rongyi <sqrongyi@163.com>
2016-10-18 14:35:01 +0800

    Add word ladder

---
+++ word-ladder.cc
@@ -0,0 +1,13 @@
+// http://www.lintcode.com/zh-cn/problem/word-ladder
+class Solution {
+public:
+  /**
+   * @param start, a string
+   * @param end, a string
+   * @param dict, a set of string
+   * @return an integer
+   */
+  int ladderLength(string start, string end, unordered_set<string> &dict) {
+
+  }
+};

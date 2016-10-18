00145e83b0f21d865c135befce5f97fdaf47499a
rongyi <sqrongyi@163.com>
2016-10-18 15:05:50 +0800

    Add word ladder ii

---
+++ word-ladder-ii.cc
@@ -0,0 +1,26 @@
+// http://www.lintcode.com/zh-cn/problem/word-ladder-ii
+#include <vector>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <queue>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+using std::unordered_set;
+using std::queue;
+
+class Solution {
+public:
+  /**
+   * @param start, a string
+   * @param end, a string
+   * @param dict, a set of string
+   * @return a list of lists of string
+   */
+  vector<vector<string>> findLadders(string start, string end, unordered_set<string> &dict) {
+
+  }
+};

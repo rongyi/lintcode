36b44c663a9801c1b0fa1a9450772484a8c9f6a5
rongyi <sqrongyi@163.com>
2016-10-18 18:36:25 +0800

    Fuck, overflow bug

--- dices-sum.cc
+++ dices-sum.cc
@@ -20,25 +20,35 @@ public:
     vector<std::pair<int, double>> ret;
     if (n <= 0)
       return ret;
-    const double total = std::pow(6, n);
-    const int target = 6 * n;
-    vector<vector<int>> dp(n + 1, vector<int>(target + 1, 0));
-    dp[0][0] = 1;
 
-    for (int i = 1; i <= n; i++) {
-      for (int j = 1; j <= target; j++) {
+    const int target = 6 * n;
+    vector<vector<long long>> dp(n, vector<long long>(target + 1, 0));
+
+    dp[0][0] = 1;
+    for (int i = 1; i <= 6; i++) {
+      dp[0][i] = 1;
+    }
+
+    for (int i = 1; i < n; i++) {
+      for (int j = i; j < i * 6 + 1; j++) {
         for (int k = 1; k <= 6; k++) {
-          if (j >= k) {
-            dp[i][j] += dp[i - 1][j - k];
-          }
+          dp[i][j + k] += dp[i - 1][j];
         }
       }
     }
 
+    double total = 0;
     for (int i = n; i <= target; i++) {
-      int cur_sum = dp[n][i];
-      double prob = cur_sum / total;
-      ret.push_back(std::make_pair(i, prob));
+      if (dp[n - 1][i] > 0)
+        total += dp[n - 1][i];
+    }
+
+    for (int i = n; i <= target; i++) {
+      if (dp[n - 1][i] > 0) {
+        double cur_sum = (dp[n - 1][i]) + 0.0;
+        double prob = cur_sum / total;
+        ret.push_back(std::make_pair(i, prob));
+      }
     }
 
     return ret;

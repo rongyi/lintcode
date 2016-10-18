baccb52f412cc75914b2d6124f7f3e27245a02fd
rongyi <sqrongyi@163.com>
2016-10-18 16:36:14 +0800

    Add comment

--- word-ladder-ii.cc
+++ word-ladder-ii.cc
@@ -22,7 +22,16 @@ public:
    * @param end, a string
    * @param dict, a set of string
    * @return a list of lists of string
-   */
+   如果要返回所有的结果，问题变复杂了些。因为用BFS相对于DFS的劣势就是不方便存储结果。这种需要返回所有结果的，还是应该从DFS考虑，但是直接应用DFS复杂
+   度会很高，因为这道题我们只要知道结尾就好了，不用继续往下搜。
+
+   所以问题就转化成怎样用DFS的同时又可以限制DFS的深度，所以我们可以BFS与DFS结合。先用BFS搜到结尾字符串，然后把中途所有的字符串及其跟起始字符的
+   edit distance存在一个map里。这样的话，我们就可以从结尾字符串开始DFS，只有Map内的字符串才考虑继续DFS，直至搜到起始字符。
+
+   注意这里有个小技巧，就是为什么不从起始字符串开始DFS直至搜到结尾字符串，而是反过来。这里可以脑子里想像一个图，如果从起始字符串开始搜，
+   到最后一层的话会有很多无效搜索，因为那层我们只需要找到结尾字符串，那么多无效的搜索到最一层太浪费时间。反之，如果我们从结尾字符串开始DFS,
+   我们把起始层控制在一个字符串，整个图先越来越宽，然后越来越窄直到起始字符串，而非一直越来越宽直到结尾字符串那层。
+  */
   vector<vector<string>> findLadders(string start, string end, unordered_set<string> &dict) {
     unordered_map<string, int> distance_map;
     mkDistanceMap(start, end, dict, distance_map);
@@ -43,6 +52,7 @@ private:
       vec.push_back(end);
       std::reverse(vec.begin(), vec.end());
       ret.push_back(vec);
+
       return;
     }
     cur.push_back(start);

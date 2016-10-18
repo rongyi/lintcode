62d90560814d648f8d6e1b6377c340c45ecf1e67
rongyi <sqrongyi@163.com>
2016-10-18 15:03:22 +0800

    Add word ladder

--- word-ladder.cc
+++ word-ladder.cc
@@ -18,7 +18,10 @@ public:
    * @param end, a string
    * @param dict, a set of string
    * @return an integer
-   */
+   1. 将每个单词看成图的一个节点。
+   2. 当单词s1改变一个字符可以变成存在于字典的单词s2时，则s1与s2之间有连接。
+   3. 给定s1和s2，问题I转化成了求在图中从s1->s2的最短路径长度。而问题II转化为了求所有s1->s2的最短路径。
+  */
   int ladderLength(string start, string end, unordered_set<string> &dict) {
     dict.insert(end);
     std::queue<std::pair<string, int>> q;
@@ -39,6 +42,8 @@ public:
   }
 
 private:
+  // 如何找到与当前节点相邻的所有节点?
+  // 遍历当前单词的每个字符x，将其改变成a~z中除x外的任意一个，形成一个新的单词，在字典中判断是否存在。复杂度为：26*w，w为单词长度。
   vector<string> findNeighbors(string s, unordered_set<string> &dict) {
     vector<string> ret;
     for (int i = 0; i < s.size(); i++) {
@@ -49,6 +54,7 @@ private:
         s[i] = 'a' + j;
         if (dict.find(s) != dict.end()) {
           ret.push_back(s);
+          // duplicate match is not allowed
           dict.erase(s);
         }
       }

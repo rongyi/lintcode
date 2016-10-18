3b658c3fcca4abd8e8952d30b4f0316481221007
rongyi <sqrongyi@163.com>
2016-10-18 10:56:40 +0800

    Add surrounded regions

---
+++ surrounded-regions.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/surrounded-regions
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param board a 2D board containing 'X' and 'O'
+   * @return void
+   */
+  void surroundedRegions(vector<vector<char>>& board) {
+
+  }
+};

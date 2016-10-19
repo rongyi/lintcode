968dd7e978c14558dbeadaf0196bc283143a449d
rongyi <sqrongyi@163.com>
2016-10-19 10:59:43 +0800

    Update binaryRepresentation

---
+++ binary-representation.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/binary-representation
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   *@param n: Given a decimal number that is passed in as a string
+   *@return: A string
+   */
+  string binaryRepresentation(string n) {
+
+  }
+};

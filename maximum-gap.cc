7d8ad19ce82f4f7c77781906e9efae3656f30436
rongyi <sqrongyi@163.com>
2016-10-19 15:10:17 +0800

    Update maximumGap

--- maximum-gap.cc
+++ maximum-gap.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using std::vector;
 using std::cout;
@@ -14,7 +15,16 @@ public:
    * @param nums: a vector of integers
    * @return: the maximum difference
    */
-  int maximumGap(vector<int> nums) {
+  int maximumGap(vector<int> &nums) {
+    if (nums.size() < 2)
+      return 0;
+    std::sort(nums.begin(), nums.end());
+    int ret = std::numeric_limits<int>::min();
+    for (int i = 1; i < nums.size(); i++) {
+      if (nums[i] - nums[i - 1] > ret)
+        ret = nums[i] - nums[i - 1];
+    }
 
+    return ret;
   }
 };

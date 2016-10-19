2d277ddbe8deaa0e17788dcd73608e0bebfeef8f
rongyi <sqrongyi@163.com>
2016-10-19 09:32:44 +0800

    Add maximalRectangle

---
+++ maximal-rectangle.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/maximal-rectangle
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param matrix a boolean 2D matrix
+   * @return an integer
+   */
+  int maximalRectangle(vector<vector<bool> > &matrix) {
+
+  }
+};

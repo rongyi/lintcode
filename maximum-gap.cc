7e2e82a56036ffae327f4791074ab1147790b726
rongyi <sqrongyi@163.com>
2016-10-19 15:06:34 +0800

    Add maximumGap

---
+++ maximum-gap.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/maximum-gap
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param nums: a vector of integers
+   * @return: the maximum difference
+   */
+  int maximumGap(vector<int> nums) {
+
+  }
+};

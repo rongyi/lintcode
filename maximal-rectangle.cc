3ee9ef85968b46a665fee69d138b5e97fc59b666
rongyi <sqrongyi@163.com>
2016-10-19 09:49:18 +0800

    Add brute fore version

--- maximal-rectangle.cc
+++ maximal-rectangle.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using std::vector;
 using std::cout;
@@ -15,6 +16,41 @@ public:
    * @return an integer
    */
   int maximalRectangle(vector<vector<bool> > &matrix) {
+    if (matrix.empty())
+      return 0;
+    const int m = matrix.size();
+    const int n = matrix[0].size();
+    int ret = 0;
+    for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        if (matrix[i][j]) {
+          auto cur_max = curMax(matrix, i, j);
+          ret = std::max(ret, cur_max);
+        }
+      }
+    }
 
+    return ret;
+  }
+
+private:
+  // brute forece
+  int curMax(vector<vector<bool>> &matrix, int row, int col) {
+    int ret = 0;
+    int min_width = std::numeric_limits<int>::max();
+
+    for (int i = row; i < matrix.size() && matrix[i][col]; i++) {
+      int cur_width = 0;
+      while (col + cur_width < matrix[row].size() &&
+             matrix[i][col + cur_width]) {
+        cur_width++;
+      }
+      min_width = std::min(min_width, cur_width);
+
+      int cur_area = min_width * (i - row + 1);
+      ret = std::max(cur_area, ret);
+    }
+
+    return ret;
   }
 };

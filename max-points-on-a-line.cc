a58a90657403a3b80609b35dfb4ccc47cb13cfc3
rongyi <sqrongyi@163.com>
2016-10-19 10:41:46 +0800

    Update maxPoints at line

--- max-points-on-a-line.cc
+++ max-points-on-a-line.cc
@@ -2,11 +2,14 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <unordered_map>
+#include <algorithm>
 
 using std::vector;
 using std::cout;
 using std::endl;
 using std::string;
+using std::unordered_map;
 
 
 /**
@@ -18,6 +21,12 @@ using std::string;
  *     Point(int a, int b) : x(a), y(b) {}
  * };
  */
+struct Point {
+  int x;
+  int y;
+  Point() : x(0), y(0) {}
+  Point(int a, int b) : x(a), y(b) {}
+};
 
 class Solution {
 public:
@@ -26,14 +35,44 @@ public:
    * @return an integer
    */
   int maxPoints(vector<Point>& points) {
+    if (points.size() < 3)
+      return points.size();
+    int ret = 0;
 
+    unordered_map<double, int> slope_count;
+    for (int i = 0; i < points.size() - 1; i++) {
+      slope_count.clear();
+
+      // 重合
+      int same_count = 0;
+      // 共线的个数
+      int point_max = 1;
+      for (int j = i + 1; j < points.size(); j++) {
+        double slope;
+        if (points[i].x == points[j].x) {
+          if (points[i].y == points[j].y) {
+            ++same_count;
+            continue;
+          }
+          slope = std::numeric_limits<double>::infinity();
+        } else {
+          slope = 1.0 * (points[i].y - points[j].y) / (points[i].x - points[j].x);
+        }
+
+        int count = 0;
+        if (slope_count.find(slope) != slope_count.end()) {
+          count = ++slope_count[slope];
+        } else {
+          count = 2;
+          slope_count[slope] = 2;
+        }
+
+        point_max = std::max(point_max, count);
+      }
+
+      ret = std::max(ret, point_max + same_count);
+    }
+    return ret;
   }
 
-private:
-  struct Point {
-    int x;
-    int y;
-    Point() : x(0), y(0) {}
-    Point(int a, int b) : x(a), y(b) {}
-  };
 };

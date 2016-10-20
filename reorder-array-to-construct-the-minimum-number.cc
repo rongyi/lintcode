9f30e64405dd70bbb2409a8b1359e2ae8f5b2f50
rongyi <sqrongyi@163.com>
2016-10-20 09:42:35 +0800

    Update reorder

--- reorder-array-to-construct-the-minimum-number.cc
+++ reorder-array-to-construct-the-minimum-number.cc
@@ -2,6 +2,8 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <algorithm>
+#include <sstream>
 
 using std::vector;
 using std::cout;
@@ -15,6 +17,32 @@ public:
    * @return a string
    */
   string minNumber(vector<int>& nums) {
+    auto cmp = [](int i, int j) {
+      auto si = std::to_string(i);
+      auto sj = std::to_string(j);
+      return si + sj < sj + si;
+    };
+    std::sort(nums.begin(), nums.end(), cmp);
+    std::stringstream ss;
+    for (auto &i : nums)
+      ss << i;
+    auto ret = ss.str();
 
+    if (ret.find_first_not_of("0") == std::string::npos) {
+      return "0";
+    }
+
+    ret = ret.substr(ret.find_first_not_of("0"));
+
+    return ret;
   }
 };
+
+int main()
+{
+  Solution so;
+  vector<int> test{0};
+  auto ret = so.minNumber(test);
+  cout << ret << endl;
+  return 0;
+}

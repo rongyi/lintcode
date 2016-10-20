3fdecd1e0f52abcf465506ee7cf9b87ee985340f
rongyi <sqrongyi@163.com>
2016-10-20 15:42:45 +0800

    Add permutationIndexII

---
+++ permutation-index-ii.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/permutation-index-ii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param A an integer array
+   * @return a long integer
+   */
+  long long permutationIndexII(vector<int>& nums) {
+
+  }
+};

d438e15ad8834b67337c5a514fcbcb98cfeace77
rongyi <sqrongyi@163.com>
2016-10-20 11:26:37 +0800

    Update permutationsII

--- permutations-ii.cc
+++ permutations-ii.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <unordered_set>
 
 using std::vector;
 using std::cout;
@@ -15,6 +16,35 @@ public:
    * @return: A list of unique permutations.
    */
   vector<vector<int> > permuteUnique(vector<int> &nums) {
+    vector<vector<int>> ret;
+    if (nums.size() == 0)
+      return vector<vector<int>>{{}};
 
+    vector<int> cur;
+    doPermute(nums, ret, cur, 0);
+
+    return ret;
+  }
+
+private:
+  void doPermute(vector<int> &nums, vector<vector<int>> &ret,
+                 vector<int> &cur, unsigned index) {
+    if (index == nums.size()) {
+      ret.push_back(cur);
+      return;
+    }
+
+    std::unordered_set<int> set;
+    for (int i = index; i < nums.size(); i++) {
+      if (set.find(nums[i]) == set.end()) {
+        set.insert(nums[i]);
+
+        std::swap(nums[index], nums[i]);
+        cur.push_back(nums[index]);
+        doPermute(nums, ret, cur, index + 1);
+        cur.pop_back();
+        std::swap(nums[index], nums[i]);
+      }
+    }
   }
 };

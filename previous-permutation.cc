d35ceaf19b05089c15c6dcbb51dde6614aaa0804
rongyi <sqrongyi@163.com>
2016-10-20 14:49:50 +0800

    Update previousPermuation

--- previous-permutation.cc
+++ previous-permutation.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 using std::vector;
 using std::cout;
@@ -15,6 +16,36 @@ public:
    * @return: An array of integers that's previous permuation
    */
   vector<int> previousPermuation(vector<int> &nums) {
+    if (nums.size() < 2)
+      return nums;
 
+    return doPermute(nums);
+  }
+private:
+  vector<int> doPermute(vector<int> &nums) {
+    const int m = nums.size();
+    int i = m - 1;
+    while (i > 0 && nums[i - 1] <= nums[i])
+      i--;
+    if (i > 0) {
+      int j = i - 1;
+      for (; j + 1 < m && nums[j + 1] <= nums[i - 1]; j++)
+        ;
+      std::swap(nums[i - 1], nums[j]);
+      std::reverse(nums.begin() + i, nums.end());
+    } else {
+      std::reverse(nums.begin(), nums.end());
+    }
+    return nums;
   }
 };
+
+int main()
+{
+  Solution so;
+  vector<int> test{1, 3, 2};
+  auto ret = so.previousPermuation(test);
+  for (auto i : ret)
+    cout << i << endl;
+  return 0;
+}

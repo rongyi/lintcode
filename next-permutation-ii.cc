449d5ac89a58b127c5a97d83d0a183d937e8cfaa
rongyi <sqrongyi@163.com>
2016-10-20 14:56:08 +0800

    Add nextPermutationii

---
+++ next-permutation-ii.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/next-permutation-ii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param nums: a vector of integers
+   * @return: return nothing (void), do not return anything, modify nums in-place instead
+   */
+  void nextPermutation(vector<int> &nums) {
+
+  }
+};

cf0e77aa0718008d5712044a67d9cec0e545db9b
rongyi <sqrongyi@163.com>
2016-10-20 14:23:16 +0800

    Add previousPermuation

---
+++ previous-permutation.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/previous-permutation
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param nums: An array of integers
+   * @return: An array of integers that's previous permuation
+   */
+  vector<int> previousPermuation(vector<int> &nums) {
+
+  }
+};

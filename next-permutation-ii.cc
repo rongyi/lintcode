3d61dead2a7845455303d91fc027be5c1df7bd0e
rongyi <sqrongyi@163.com>
2016-10-20 15:41:46 +0800

    Update nextPermutation

--- next-permutation-ii.cc
+++ next-permutation-ii.cc
@@ -14,7 +14,10 @@ public:
   /**
    * @param nums: a vector of integers
    * @return: return nothing (void), do not return anything, modify nums in-place instead
-   */
+   1. 从低位向高位（从右向左）找第一个递减的数：s[i]<s[i+1]。如果不存在，则表明该permutation已经最大，next permutation为当前序列的逆序。
+   2. 在s[i+1:n-1]中找一个j，使s[j]>s[i]>=s[j+1]，swap(s[i], s[j])
+   3. 将s[i+1:n-1]排序，从低位到高位单调递减。
+  */
   void nextPermutation(vector<int> &nums) {
     int j = -1;
     int k = 0;

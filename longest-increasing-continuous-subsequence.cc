90551fbd7eecd8d6174d296ae640109e46460fc5
rongyi <sqrongyi@163.com>
2016-10-24 17:36:08 +0800

    Add longestIncreasingContinuousSubsequence

---
+++ longest-increasing-continuous-subsequence.cc
@@ -0,0 +1,11 @@
+// http://www.lintcode.com/zh-cn/problem/longest-increasing-continuous-subsequence
+class Solution {
+public:
+  /**
+   * @param A an array of Integer
+   * @return  an integer
+   */
+  int longestIncreasingContinuousSubsequence(vector<int>& A) {
+
+  }
+};

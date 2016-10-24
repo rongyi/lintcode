48af81e7561ead0d1ee45fc39f977d1d1eafd461
rongyi <sqrongyi@163.com>
2016-10-24 18:14:10 +0800

    Add minPathSum

---
+++ minimum-path-sum.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/minimum-path-sum
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param grid: a list of lists of integers.
+   * @return: An integer, minimizes the sum of all numbers along its path
+   */
+  int minPathSum(vector<vector<int> > &grid) {
+
+  }
+};

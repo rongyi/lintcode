cd391fdf2d17ff7c28ab40a09196dd0c3184568d
rongyi <sqrongyi@163.com>
2016-10-24 19:44:12 +0800

    Update productExcludeItself

--- product-of-array-exclude-itself.cc
+++ product-of-array-exclude-itself.cc
@@ -15,6 +15,30 @@ public:
    * @return: A long long array B and B[i]= A[0] * ... * A[i-1] * A[i+1] * ... * A[n-1]
    */
   vector<long long> productExcludeItself(vector<int> &nums) {
+    vector<long long> ret;
+    if (nums.empty())
+      return ret;
+    if (nums.size() == 1) {
+      ret.push_back(1);
+      return ret;
+    }
+    const int m = nums.size();
+    vector<long long> left(m, 0);
+    vector<long long> right(m, 0);
+    // left
+    left[0] = 1;
+    for (int i = 1; i < m; i++) {
+      left[i] = left[i - 1] * nums[i - 1];
+    }
+    // right
+    right[m - 1] = 1;
+    for (int i = m - 2; i >= 0; --i) {
+      right[i] = right[i + 1] * nums[i + 1];
+    }
+    for (int i = 0; i < m; i++) {
+      ret.push_back(left[i] * right[i]);
+    }
 
+    return ret;
   }
 };

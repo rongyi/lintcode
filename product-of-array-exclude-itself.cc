32314c2de7f5cb7ac5f66d1f53740c3f117b2a9f
rongyi <sqrongyi@163.com>
2016-10-24 19:34:08 +0800

    Add productExcludeItself

---
+++ product-of-array-exclude-itself.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/product-of-array-exclude-itself
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param A: Given an integers array A
+   * @return: A long long array B and B[i]= A[0] * ... * A[i-1] * A[i+1] * ... * A[n-1]
+   */
+  vector<long long> productExcludeItself(vector<int> &nums) {
+
+  }
+};

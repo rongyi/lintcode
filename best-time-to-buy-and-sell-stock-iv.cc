b06ccdbbef53361940f19178bf75b89fc83831c8
rongyi <sqrongyi@163.com>
2016-10-24 17:15:36 +0800

    Update maxProfit

---
+++ best-time-to-buy-and-sell-stock-iv.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/best-time-to-buy-and-sell-stock-iv
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param k: An integer
+   * @param prices: Given an integer array
+   * @return: Maximum profit
+   */
+  int maxProfit(int k, vector<int> &prices) {
+
+  }
+};

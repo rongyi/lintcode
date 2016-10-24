6e47e478111ea4bb904551562fb7950563a0fa74
rongyi <sqrongyi@163.com>
2016-10-24 10:11:37 +0800

    Add letterCombinations

---
+++ letter-combinations-of-a-phone-number.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/letter-combinations-of-a-phone-number
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param digits A digital string
+   * @return all posible letter combinations
+   */
+  vector<string> letterCombinations(string& digits) {
+
+  }
+};

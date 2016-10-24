60a0405269382bba1f0ff735ea4fabbe206667e0
rongyi <sqrongyi@163.com>
2016-10-24 18:58:12 +0800

    Add triangle

---
+++ triangle.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/triangle
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param triangle: a list of lists of integers.
+   * @return: An integer, minimum path sum.
+   */
+  int minimumTotal(vector<vector<int> > &triangle) {
+
+  }
+};

fdd56e26b1af99559209e970fe6a8400092bccf2
rongyi <sqrongyi@163.com>
2016-10-25 10:09:21 +0800

    Update paint fence

--- paint-fence.cc
+++ paint-fence.cc
@@ -1,4 +1,13 @@
 // http://www.lintcode.com/zh-cn/problem/paint-fence
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
 class Solution {
 public:
   /**
@@ -7,6 +16,14 @@ public:
    * @return an integer, the total number of ways
    */
   int numWays(int n, int k) {
-
+    if (n == 0 || k == 0)
+      return 0;
+    vector<int> dp(n, 0);
+    dp[0] = k;
+    dp[1] = k * k;
+    for (int i = 2; i < n; i++) {
+      dp[i] = dp[i - 2] * (k - 1) + dp[i - 1] * (k - 1);
+    }
+    return dp[n - 1];
   }
 };

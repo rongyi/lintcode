43aa3bda1c3a313f2cef64aff487fac2bad87074
rongyi <sqrongyi@163.com>
2016-10-25 09:49:56 +0800

    Add paint fence

---
+++ paint-fence.cc
@@ -0,0 +1,12 @@
+// http://www.lintcode.com/zh-cn/problem/paint-fence
+class Solution {
+public:
+  /**
+   * @param n non-negative integer, n posts
+   * @param k non-negative integer, k colors
+   * @return an integer, the total number of ways
+   */
+  int numWays(int n, int k) {
+
+  }
+};

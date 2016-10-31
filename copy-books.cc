294705a837c2fea6e5d0f27f821422958fff713b
rongyi <sqrongyi@163.com>
2016-10-31 17:47:37 +0800

    Add copyBooks

---
+++ copy-books.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/copy-books
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param pages: a vector of integers
+   * @param k: an integer
+   * @return: an integer
+   */
+  int copyBooks(vector<int> &pages, int k) {
+    // write your code here
+  }
+};

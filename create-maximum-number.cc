8293bf8e8f468e7093721b415d327c326db1d7d0
rongyi <sqrongyi@163.com>
2016-11-01 09:07:06 +0800

    Add create maxNumber

---
+++ create-maximum-number.cc
@@ -0,0 +1,22 @@
+// http://www.lintcode.com/zh-cn/problem/create-maximum-number
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param nums1 an integer array of length m with digits 0-9
+   * @param nums2 an integer array of length n with digits 0-9
+   * @param k an integer and k <= m + n
+   * @return an integer array
+   */
+  vector<int> maxNumber(vector<int>& nums1, vector<int>& nums2, int k) {
+
+  }
+};

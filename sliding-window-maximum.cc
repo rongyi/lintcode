c72ab6841824fb7573d424b6326244a8752643c5
rongyi <sqrongyi@163.com>
2016-11-01 10:31:02 +0800

    Update sliding window maximum

--- sliding-window-maximum.cc
+++ sliding-window-maximum.cc
@@ -2,11 +2,13 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <queue>
 
 using std::vector;
 using std::cout;
 using std::endl;
 using std::string;
+using std::deque;
 
 class Solution {
 public:
@@ -15,6 +17,18 @@ public:
    * @return: The maximum number inside the window at each moving.
    */
   vector<int> maxSlidingWindow(vector<int> &nums, int k) {
+    vector<int> ret;
+    deque<int> q;
+    for (unsigned i = 0; i < nums.size(); i++) {
+      if (!q.empty() && q.front() == i - k)
+        q.pop_front();
+      while (!q.empty() && nums[q.back()] < nums[i])
+        q.pop_back();
+      q.push_back(i);
+      if (i >= k - 1)
+        ret.push_back(nums[q.front()]);
+    }
 
+    return ret;
   }
 };

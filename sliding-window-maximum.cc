a29a4b2f4f006b884ab97a279a77f62acfa5d914
rongyi <sqrongyi@163.com>
2016-11-01 10:28:02 +0800

    Add sliding window

---
+++ sliding-window-maximum.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/sliding-window-maximum
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param nums: A list of integers.
+   * @return: The maximum number inside the window at each moving.
+   */
+  vector<int> maxSlidingWindow(vector<int> &nums, int k) {
+
+  }
+};

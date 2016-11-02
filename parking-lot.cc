1d117d0cf5235c936f4517b99090eea591beae1f
rongyi <sqrongyi@163.com>
2016-11-02 08:38:21 +0800

    Update templdate

--- parking-lot.cc
+++ parking-lot.cc
@@ -16,23 +16,23 @@ enum class VehicleSize {
 };
 
 class Vehicle {
-  // Write your code here
+
 };
 
 class Bus: public Vehicle {
-  // Write your code here
+
 };
 
 class Car: public Vehicle {
-  // Write your code here
+
 };
 
 class Motorcycle: public Vehicle {
-  // Write your code here
+
 };
 
 class Level {
-  // Write your code here
+
 };
 
 class ParkingLot {
@@ -41,17 +41,17 @@ public:
   // @param num_rows  each level has num_rows rows of spots
   // @param spots_per_row each row has spots_per_row spots
   ParkingLot(int n, int num_rows, int spots_per_row) {
-    // Write your code here
+
   }
 
   // Park the vehicle in a spot (or multiple spots)
   // Return false if failed
   bool parkVehicle(Vehicle &vehicle) {
-    // Write your code here
+
   }
 
   // unPark the vehicle
   void unParkVehicle(Vehicle &vehicle) {
-    // Write your code here
+
   }
 };

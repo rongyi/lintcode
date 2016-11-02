11d2aa1f8ceabda0a255006a7b0460612b9e1971
rongyi <sqrongyi@163.com>
2016-11-02 14:30:46 +0800

    Update building outline

---
+++ building-outline.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/building-outline
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param buildings: A list of lists of integers
+   * @return: Find the outline of those buildings
+   */
+  vector<vector<int> > buildingOutline(vector<vector<int> > &buildings) {
+
+  }
+};

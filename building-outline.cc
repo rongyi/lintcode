71123f64f0b30e31bee1868705d2917f37a98560
rongyi <sqrongyi@163.com>
2016-11-02 15:03:43 +0800

    Update buildingOutline

--- building-outline.cc
+++ building-outline.cc
@@ -2,11 +2,16 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <set>
+#include <algorithm>
+#include <map>
 
 using std::vector;
 using std::cout;
 using std::endl;
 using std::string;
+using std::multiset;
+using std::pair;
 
 class Solution {
 public:
@@ -14,7 +19,38 @@ public:
    * @param buildings: A list of lists of integers
    * @return: Find the outline of those buildings
    */
-  vector<vector<int> > buildingOutline(vector<vector<int> > &buildings) {
+  vector<vector<int>> buildingOutline(vector<vector<int>> &buildings) {
+    vector<vector<int>> ret;
+    for (auto x : buildings) {
+      points_.insert(x[0]);
+      points_.insert(x[1]);
+      start_[x[0]].push_back(x[2]);
+      end_[x[1]].push_back(x[2]);
+    }
 
+    int pre = 0;
+    heap_.insert(0);
+    for (auto cur : points_)
+      {
+        auto prev = *heap_.rbegin();
+        if (prev) {
+          if (ret.empty() || prev != ret[ret.size() - 1][2])
+            ret.push_back(vector<int>{pre, cur, prev});
+          else
+            ret[ret.size() - 1][1] = cur;
+        }
+        for (auto x : start_[cur]) heap_.insert(x);
+        for (auto x : end_[cur]) heap_.erase(heap_.find(x));
+        pre = cur;
+      }
+
+    return ret;
   }
+private:
+  std::set<int> points_;
+
+  std::map<int,vector<int>> start_;
+  std::map<int, vector<int>> end_;
+
+  multiset<int> heap_;
 };

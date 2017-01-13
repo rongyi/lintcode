eeb0d822feadc04396e0bd82b9ed5b694f61c00d
rongyi <sqrongyi@163.com>
2017-01-13 16:33:47 +0800

    Update lfu cache template

---
+++ src/lfu-cache.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/lfu-cache
+class LFUCache{
+public:
+  // @param capacity, an integer
+  LFUCache(int capacity) {
+    // Write your code here
+
+  }
+
+  // @param key, an integer
+  // @param value, an integer
+  // @return nothing
+  void set(int key, int value) {
+    // Write your code here
+  }
+
+  // @return an integer
+  int get(int key) {
+    // Write your code here
+  }
+};

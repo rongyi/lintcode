8b10ae99376ebe669b648b66b2a554373fcec816
rongyi <sqrongyi@163.com>
2017-03-03 09:29:54 +0800

    Add template of maxNode

---
+++ src/binary-tree-maximum-node.cc
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+    /**
+     * @param root the root of binary tree
+     * @return the max node
+     */
+    TreeNode* maxNode(TreeNode* root) {
+        // Write your code here
+    }
+};

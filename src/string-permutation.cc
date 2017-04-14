c1d657f64b4d4d11462387bc53c2d00343a17fbd
rongyi <sqrongyi@163.com>
2017-04-14 16:32:30 +0800

    Add template for string permutation

---
+++ src/string-permutation.cc
@@ -0,0 +1,19 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+class Solution {
+public:
+  /**
+   * @param A a string
+   * @param B a string
+   * @return a boolean
+   */
+  bool stringPermutation(string& A, string& B) {
+
+  }
+};

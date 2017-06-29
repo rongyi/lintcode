846832f9636a3894433ba5bec7483e7943933f35
rongyi <rongyi3@wanda.cn>
2017-06-29 13:24:39 +0800

    Add lengthLongestPath

---
+++ src/system-longest-file-path.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/system-longest-file-path
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param input an abstract file system
+   * @return return the length of the longest absolute path to file
+   */
+  int lengthLongestPath(string& input) {
+    // Write your code here
+  }
+};

41c233ae31d3c2d464ed00e7baf7f16343693b7c
rongyi <sqrongyi@163.com>
2017-07-01 21:36:16 +0800

    Add problem template

---
+++ src/partition-equal-subset-sum.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/partition-equal-subset-sum
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param nums a non-empty array only positive integers
+   * @return return true if can partition or false
+   */
+  bool canPartition(vector<int>& nums) {
+    // Write your code here
+  }
+};

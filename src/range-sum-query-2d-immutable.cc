770085c463c7ada6e8d3f08e015e32306fd6c1e5
rongyi <sqrongyi@163.com>
2017-07-02 19:24:20 +0800

    Update NumMatrix

--- src/range-sum-query-2d-immutable.cc
+++ src/range-sum-query-2d-immutable.cc
@@ -11,12 +11,22 @@ using std::string;
 class NumMatrix {
 public:
   NumMatrix(vector<vector<int>> matrix) {
+    if (matrix.empty() || matrix[0].empty())
+      return;
+    dp_.resize(matrix.size() + 1, vector<int>(matrix[0].size() + 1, 0));
 
+    for (int i = 1; i <= matrix.size(); ++i) {
+      for (int j = 1; j<= matrix[0].size(); ++j) {
+        dp_[i][j] = dp_[i][j - 1] + dp_[i - 1][j] - dp_[i - 1][j - 1] + matrix[i - 1][j - 1];
+      }
+    }
   }
 
   int sumRegion(int row1, int col1, int row2, int col2) {
-
+    return  dp_[row2 + 1][col2 + 1] - dp_[row1][col2 + 1] - dp_[row2 + 1][col1] + dp_[row1][col1];
   }
+private:
+  vector<vector<int>> dp_;
 };
 
 /**

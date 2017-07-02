070ed6efd9050e1ae12680447a7e69671bb00027
rongyi <sqrongyi@163.com>
2017-07-02 09:57:15 +0800

    Add new

---
+++ src/course-schedule-ii.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/course-schedule-ii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param numCourses a total of n courses
+   * @param prerequisites a list of prerequisite pairs
+   * @return the course order
+   */
+  vector<int> findOrder(int numCourses, vector<pair<int, int>>& prerequisites) {
+
+  }
+};

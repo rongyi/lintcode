ac9286737c5b09387603aeb51e6ee5f433d1e77f
rongyi <sqrongyi@163.com>
2017-07-02 18:29:55 +0800

    Add NumMatrix

---
+++ src/range-sum-query-2d-immutable.cc
@@ -0,0 +1,26 @@
+// http://www.lintcode.com/zh-cn/problem/range-sum-query-2d-immutable
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class NumMatrix {
+public:
+  NumMatrix(vector<vector<int>> matrix) {
+
+  }
+
+  int sumRegion(int row1, int col1, int row2, int col2) {
+
+  }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */

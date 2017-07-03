6d79518e79a73f53c8b56fa46527784107ad1830
rongyi <rongyi3@wanda.cn>
2017-07-03 08:40:49 +0800

    Add min max problem

---
+++ src/guess-number-game-ii.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/guess-number-game-ii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param n an integer
+   * @return how much money you need to have to guarantee a win
+   */
+  int getMoneyAmount(int n) {
+
+  }
+};

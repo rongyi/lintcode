4ca27f5cf4e3d047186c473a7ba599cb45fb024e
rongyi <rongyi3@wanda.cn>
2017-07-03 08:44:12 +0800

    Update minmax

--- src/guess-number-game-ii.cc
+++ src/guess-number-game-ii.cc
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <limits>
 
 using std::vector;
 using std::cout;
@@ -15,6 +16,16 @@ public:
    * @return how much money you need to have to guarantee a win
    */
   int getMoneyAmount(int n) {
+    vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+    for (int l = n - 1; l > 0; --l) {
+      for (int r = l + 1; r <= n; ++r) {
+        dp[l][r] = std::numeric_limits<int>::max();
+        for (int i = l; i < r; i++) {
+          dp[l][r] = std::min(dp[l][r], i + std::max(dp[l][i - 1], dp[i + 1][r]));
+        }
+      }
+    }
 
+    return dp[1][n];
   }
 };

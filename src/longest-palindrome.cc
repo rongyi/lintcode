53a332d87bb75001a8932d99daaf978d912dc5eb
rongyi <rongyi3@wanda.cn>
2017-07-18 18:18:12 +0800

    Add calculate longestPalindrome

---
+++ src/longest-palindrome.cc
@@ -0,0 +1,39 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param s a string which consists of lowercase or uppercase letters
+   * @return the length of the longest palindromes that can be built
+   */
+  int longestPalindrome(string& s) {
+    std::unordered_map<char, int> stat;
+    for (auto &c : s) {
+      stat[c]++;
+    }
+    bool has_odd = false;
+    int ret = 0;
+    for (auto iter = stat.cbegin(); iter != stat.cend(); iter++) {
+      // const char c = iter->first;
+      const int count = iter->second;
+      if (count & 0x01) {
+        ret += (count - 1);
+        if (!has_odd) {
+          ret += 1;
+          has_odd = true;
+        }
+      } else {
+        ret += count;
+      }
+    }
+    return ret;
+  }
+};

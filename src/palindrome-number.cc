e77b22745eee195b620e69c46f855cdb828aa3de
rongyi <rongyi3@wanda.cn>
2017-07-18 18:40:33 +0800

    Update palindromeNumber

--- src/palindrome-number.cc
+++ src/palindrome-number.cc
@@ -14,11 +14,44 @@ public:
    * @param num a positive number
    * @return true if it's a palindrome or false
    */
-  bool palindromeNumber(int num) {
+  bool palindromeNumberStringMethod(int num) {
     string s = std::to_string(num);
     string rs = s;
     std::reverse(rs.begin(), rs.end());
 
     return s == rs;
   }
+
+  // math way
+  bool palindromeNumber(int num) {
+    int count = 1;
+    int copy = num;
+    while (num /= 10) {
+      count *= 10;
+    }
+
+    num = copy;
+
+    while (num) {
+      int left = num / count;
+      int right = num % 10;
+      if (left != right) {
+        return false;
+      }
+
+      num = num % count;
+      num /= 10;
+      count /= 100;
+    }
+
+    return true;
+  }
 };
+
+int main()
+{
+  Solution so;
+  auto ret = so.palindromeNumber(122221);
+  cout << ret << endl;
+  return 0;
+}

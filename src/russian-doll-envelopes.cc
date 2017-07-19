02209393e8ec41522bf10c9acff432602354f640
rongyi <rongyi3@wanda.cn>
2017-07-20 07:53:55 +0800

    Add new template

---
+++ src/russian-doll-envelopes.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/russian-doll-envelopes
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param envelopes a number of envelopes with widths and heights
+   * @return the maximum number of envelopes
+   */
+  int maxEnvelopes(vector<pair<int, int>>& envelopes) {
+
+  }
+};

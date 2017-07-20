83b81b3128e7134af54849fed512fd4afe020f25
rongyi <rongyi3@wanda.cn>
2017-07-20 15:50:30 +0800

    Add isOneEditDistance

---
+++ src/edit-distance-ii.cc
@@ -0,0 +1,12 @@
+// http://www.lintcode.com/zh-cn/problem/edit-distance-ii
+class Solution {
+public:
+  /**
+   * @param s a string
+   * @param t a string
+   * @return true if they are both one edit distance apart or false
+   */
+  bool isOneEditDistance(string& s, string& t) {
+
+  }
+};

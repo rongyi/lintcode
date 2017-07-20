49bface27d212daf122c248774c9384cc4214709
rongyi <rongyi3@wanda.cn>
2017-07-20 16:11:03 +0800

    Update isOneEditDistance

--- src/edit-distance-ii.cc
+++ src/edit-distance-ii.cc
@@ -1,4 +1,14 @@
 // http://www.lintcode.com/zh-cn/problem/edit-distance-ii
+#include <vector>
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
 class Solution {
 public:
   /**
@@ -6,7 +16,34 @@ public:
    * @param t a string
    * @return true if they are both one edit distance apart or false
    */
-  bool isOneEditDistance(string& s, string& t) {
-
+  bool isOneEditDistance(string &s, string &t) {
+    // assume s is the short one
+    if (s.size() > t.size()) {
+      std::swap(s, t);
+    }
+    const int len_delta = t.size() - s.size();
+    if (len_delta > 2) {
+      return false;
+    }
+    bool ret = true;
+    if (len_delta == 1) {
+      for (int i = 0; i < s.size(); ++i) {
+        if (s[i] != t[i]) {
+          ret = s.substr(i) == t.substr(i + 1);
+          break;
+        }
+      }
+    } else if (len_delta == 0) {
+      int diff_count = 0;
+      for (int i = 0; i < s.size(); ++i) {
+        if (s[i] != t[i]) {
+          diff_count++;
+          if (diff_count > 1)
+            break;
+        }
+      }
+      ret = (diff_count == 1);
+    }
+    return ret;
   }
 };

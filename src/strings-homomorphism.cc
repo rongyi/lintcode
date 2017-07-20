98e6e7b393ca0b77b055fbb13aad6300340825d3
rongyi <rongyi3@wanda.cn>
2017-07-20 09:34:49 +0800

    Update isIsomorphic

--- src/strings-homomorphism.cc
+++ src/strings-homomorphism.cc
@@ -1,4 +1,15 @@
 // http://www.lintcode.com/zh-cn/problem/strings-homomorphism
+#include <vector>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <algorithm>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
 class Solution {
 public:
   /**
@@ -8,6 +19,33 @@ public:
    * can be replaced to get t or false
    */
   bool isIsomorphic(string &s, string &t) {
+    vector<int> sc;
+    vector<int> tc;
+    std::unordered_map<char, int> sd;
+    std::unordered_map<char, int> td;
 
+    for (auto &c : s) {
+      sd[c]++;
+    }
+    for (auto &c : t) {
+      td[c]++;
+    }
+
+    for (auto iter = sd.begin(); iter != sd.end(); iter++) {
+      sc.push_back(iter->second);
+    }
+
+    for (auto iter = td.begin(); iter != td.end(); iter++) {
+      tc.push_back(iter->second);
+    }
+    std::sort(sc.begin(), sc.end());
+    std::sort(tc.begin(), tc.end());
+
+    for (int i = 0 ; i < sc.size(); ++i) {
+      if (sc[i] != tc[i])
+        return false;
+    }
+
+    return true;
   }
 };

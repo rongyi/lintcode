7e507cf3b71ccbc021943d32bac43ca1f995497d
rongyi <rongyi3@wanda.cn>
2017-07-20 16:21:06 +0800

    Add medianSlidingWindow

---
+++ src/sliding-window-median.cc
@@ -0,0 +1,11 @@
+// http://www.lintcode.com/zh-cn/problem/sliding-window-median
+class Solution {
+public:
+  /**
+   * @param nums: A list of integers.
+   * @return: The median of the element inside the window at each moving
+   */
+  vector<int> medianSlidingWindow(vector<int> &nums, int k) {
+
+  }
+};

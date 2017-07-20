6ec866e4e13ebba7c6fdab88f3136211b3ed60f8
rongyi <rongyi3@wanda.cn>
2017-07-20 09:35:34 +0800

    Add template

---
+++ src/substring-anagrams.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/substring-anagrams
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param s a string
+   * @param p a non-empty string
+   * @return a list of index
+   */
+  vector<int> findAnagrams(string& s, string& p) {
+
+  }
+};

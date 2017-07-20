49adbd4fbb6e9bcb05ac19d1e3749a691dc30183
rongyi <rongyi3@wanda.cn>
2017-07-20 09:24:18 +0800

    Add isIsomorphic template

---
+++ src/strings-homomorphism.cc
@@ -0,0 +1,13 @@
+// http://www.lintcode.com/zh-cn/problem/strings-homomorphism
+class Solution {
+public:
+  /**
+   * @param s a string
+   * @param t a string
+   * @return true if the characters in s
+   * can be replaced to get t or false
+   */
+  bool isIsomorphic(string &s, string &t) {
+
+  }
+};

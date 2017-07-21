dc9368d299ade8a14c443ed0f6462f37710bdda1
rongyi <rongyi3@wanda.cn>
2017-07-21 13:50:01 +0800

    Add trapRainWater 3d

---
+++ src/trapping-rain-water-ii.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/trapping-rain-water-ii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param heights: a matrix of integers
+   * @return: an integer
+   */
+  int trapRainWater(vector<vector<int>> &heights) {
+
+  }
+};

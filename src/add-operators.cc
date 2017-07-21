938b65b5c077b4fcbf3bd6d7f2b670d207677935
rongyi <rongyi3@wanda.cn>
2017-07-21 09:47:16 +0800

    Add addOperators

---
+++ src/add-operators.cc
@@ -0,0 +1,21 @@
+// http://www.lintcode.com/zh-cn/problem/add-operators
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /**
+   * @param num a string contains only digits 0-9
+   * @param target an integer
+   * @return return all possibilities
+   */
+  vector<string> addOperators(string &num, int target) {
+
+  }
+};

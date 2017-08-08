e2060f768d50f016e12e8983445d2bf368b730b9
rongyi <rongyi3@wanda.cn>
2017-08-08 09:45:02 +0800

    Update description

--- src/longest-substring-without-repeating-characters.cc
+++ src/longest-substring-without-repeating-characters.cc
@@ -16,10 +16,16 @@ public:
   /**
    * @param s: a string
    * @return: an integer
+
+left标记的更新
+q p x r j x k l t z y x
+|--------|
+      |--------------|
+            |--------|
    */
   int lengthOfLongestSubstring(string s) {
     // left用以记录合法的最远左边界位置，last记录字符上一次出现的位置
-    int ans = 0, left = 0, len = s.length();
+    int ret = 0, left = 0, len = s.length();
     int last[255];
     std::memset(last, -1, sizeof last);
 
@@ -28,9 +34,9 @@ public:
       if (last[s[i]] >= left)
         left = last[s[i]] + 1;
       last[s[i]] = i;
-      ans = std::max(ans, i - left + 1);
+      ret = std::max(ret, i - left + 1);
     }
-    return ans;
+    return ret;
   }
 };
 

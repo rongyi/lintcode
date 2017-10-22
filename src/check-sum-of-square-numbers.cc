6001555deaa6c0c7b56a80ec73592a3c0303e322
rongyi <sqrongyi@163.com>
2017-10-23 07:52:02 +0800

    Add template

---
+++ src/check-sum-of-square-numbers.cc
@@ -0,0 +1,23 @@
+// http://www.lintcode.com/zh-cn/problem/check-sum-of-square-numbers
+#include <vector>
+#include <iostream>
+#include <string>
+#include <bitset>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /*
+  * @param : the given number
+  * @return: whether whether there're two integers
+  */
+  bool checkSumOfSquareNumbers(int num) {
+    if (num <= 0) {
+      return false;
+    }
+  }
+};

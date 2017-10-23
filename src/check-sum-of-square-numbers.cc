e36fa05b5f44ba0911c0fe84cfac345229bb1f69
rongyi <sqrongyi@163.com>
2017-10-23 08:39:06 +0800

    Update checkSumOfSquareNumbers

--- src/check-sum-of-square-numbers.cc
+++ src/check-sum-of-square-numbers.cc
@@ -1,8 +1,9 @@
 // http://www.lintcode.com/zh-cn/problem/check-sum-of-square-numbers
-#include <vector>
+#include <bitset>
+#include <cmath>
 #include <iostream>
 #include <string>
-#include <bitset>
+#include <vector>
 
 using std::vector;
 using std::cout;
@@ -16,8 +17,35 @@ public:
   * @return: whether whether there're two integers
   */
   bool checkSumOfSquareNumbers(int num) {
-    if (num <= 0) {
+    if (num < 0) {
       return false;
     }
+    if (num % 4 == 3)
+      return false;
+    auto lower = int(::sqrt(num));
+
+    int i = lower;
+    if (i * i == num) {
+      return true;
+    }
+    int j = 1;
+    while (j <= i) {
+      auto check = i * i + j * j;
+      if (check == num) {
+        return true;
+      } else if (check < num) {
+        j++;
+      } else {
+        i--;
+      }
+    }
+    return false;
   }
 };
+
+int main() {
+  Solution so;
+  auto ret = so.checkSumOfSquareNumbers(2082279424);
+  cout << ret << endl;
+  return 0;
+}

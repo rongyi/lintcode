d59b8caa61c425b9768b5d7b83754192479b382c
rongyi <sqrongyi@163.com>
2017-10-25 08:04:45 +0800

    Add missingString template

---
+++ src/missing-string.cc
@@ -0,0 +1,21 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <bitset>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /*
+  * @param : a given string
+  * @param : another given string
+  * @return: An array of missing string
+  */
+  vector<string> missingString(string str1, string str2) {
+
+  }
+};

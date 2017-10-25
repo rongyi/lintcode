6218f1c75e9e3b8ed8d1189f53b80cad3d29ccbe
rongyi <sqrongyi@163.com>
2017-10-25 09:32:41 +0800

    Add splitString

--- src/split-string.cc
+++ src/split-string.cc
@@ -1,1 +1,58 @@
 // http://www.lintcode.com/zh-cn/problem/split-string
+#include <vector>
+#include <iostream>
+#include <string>
+#include <bitset>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /*
+  * @param : a string to be split
+  * @return: all possible split string array
+  */
+  vector<vector<string>> splitString(string& s) {
+    vector<vector<string>> ret;
+    vector<string> cur;
+    rec(s, s.length(), 0, ret, cur);
+
+    return ret;
+  }
+
+  void rec(const string &s, const int len, int i, vector<vector<string>> &ret, vector<string> &cur) {
+    if (i >= len) {
+      vector<string> tmp = cur;
+      ret.push_back(tmp);
+      return;
+    }
+
+    cur.push_back(s.substr(i, 1));
+    rec(s, len, i + 1, ret, cur);
+    cur.pop_back();
+
+    if (i + 2 <= len) {
+      cur.push_back(s.substr(i, 2));
+      rec(s, len, i + 2, ret, cur);
+      cur.pop_back();
+    }
+  }
+};
+
+int main()
+{
+  Solution so;
+  string s = "123";
+  auto ret = so.splitString(s);
+  for (auto ss: ret) {
+    for (auto s : ss) {
+      cout << s << "  ";
+    }
+    cout << endl;
+  }
+
+  return 0;
+}

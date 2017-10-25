16486060f1248e553d24747a6f42d10ada8be001
rongyi <sqrongyi@163.com>
2017-10-25 18:44:47 +0800

    Add concatenetedString

---
+++ src/concatenated-string-with-uncommon-characters-of-two-strings.cc
@@ -0,0 +1,22 @@
+// http://www.lintcode.com/zh-cn/problem/concatenated-string-with-uncommon-characters-of-two-strings
+#include <vector>
+#include <iostream>
+#include <string>
+#include <bitset>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /*
+  * @param : the 1st string
+  * @param : the 2nd string
+  * @return: uncommon characters of given strings
+  */
+  string concatenetedString(string &s1, string &s2) {
+
+  }
+};

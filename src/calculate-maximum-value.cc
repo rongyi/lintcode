8cea7515ce81694afeb85a54d6202de73cd78696
rongyi <sqrongyi@163.com>
2017-10-30 08:44:29 +0800

    Add common header

--- src/calculate-maximum-value.cc
+++ src/calculate-maximum-value.cc
@@ -1,4 +1,13 @@
 // http://www.lintcode.com/zh-cn/problem/calculate-maximum-value
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
 
 class Solution {
 public:

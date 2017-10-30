b7c44d50317736b6c1e3f0c31210b5b0d0d5c4da
rongyi <sqrongyi@163.com>
2017-10-30 08:44:10 +0800

    Add calcMaxValue

---
+++ src/calculate-maximum-value.cc
@@ -0,0 +1,12 @@
+// http://www.lintcode.com/zh-cn/problem/calculate-maximum-value
+
+class Solution {
+public:
+  /*
+  * @param : the given string
+  * @return: the maximum value
+  */
+  int calcMaxValue(string &str) {
+
+  }
+};

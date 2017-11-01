551c9e48bd37b1b41826f32dbc27c8829176dad1
rongyi <sqrongyi@163.com>
2017-11-01 08:45:32 +0800

    Add TLE version

---
+++ src/convert-palindrome.cc
@@ -0,0 +1,63 @@
+// http://www.lintcode.com/zh-cn/problem/convert-palindrome
+#include <algorithm>
+#include <bitset>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /*
+  * @param : String
+  * @return: String
+  */
+  string convertPalindrome(string str) {
+    if (isPalindrome(str)) {
+      return str;
+    }
+    string fixs = str.substr(1);
+    std::reverse(fixs.begin(), fixs.end());
+    for (int i = 0; i < fixs.size() - 1; i++) {
+      string fix = fixs.substr(0, i + 1);
+      string concat = fix + str;
+      if (isPalindrome(concat)) {
+        return concat;
+      }
+    }
+    // last one
+    return fixs + str;
+  }
+
+private:
+  bool isPalindrome(string &s) {
+    std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+    auto left = s.begin();
+    auto right = std::prev(s.end());
+    while (left < right) {
+      if (!::isalnum(*left))
+        ++left;
+      else if (!::isalnum(*right))
+        --right;
+      else if (*left != *right)
+        return false;
+      else {
+        left++;
+        right--;
+      }
+    }
+    return true;
+  }
+};
+
+int main()
+{
+  Solution so;
+  string t{"abcd"};
+  so.convertPalindrome(t);
+  return 0;
+}

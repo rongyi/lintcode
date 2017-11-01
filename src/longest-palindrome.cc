9036d14aa03d5d75677e5ad9aa86a9c613e86416
rongyi <sqrongyi@163.com>
2017-11-01 08:19:10 +0800

    Add more comment

--- src/longest-palindrome.cc
+++ src/longest-palindrome.cc
@@ -1,3 +1,4 @@
+// http://lintcode.com/en/problem/longest-palindrome/
 #include <iostream>
 #include <string>
 #include <unordered_map>
@@ -13,6 +14,8 @@ public:
   /**
    * @param s a string which consists of lowercase or uppercase letters
    * @return the length of the longest palindromes that can be built
+   * the order isn't matter, so pick all even char and one odd char if has any
+   * even
    */
   int longestPalindrome(string &s) {
     std::unordered_map<char, int> stat;
@@ -24,13 +27,13 @@ public:
     for (auto iter = stat.cbegin(); iter != stat.cend(); iter++) {
       // const char c = iter->first;
       const int count = iter->second;
-      if (count & 0x01) {
+      if (count & 0x01) {       // select even part
         ret += (count - 1);
         if (!has_odd) {
-          ret += 1;
+          ret += 1;             // and only one odd char
           has_odd = true;
         }
-      } else {
+      } else {                  // select all even
         ret += count;
       }
     }

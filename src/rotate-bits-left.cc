619bf1b8b821339579802128844c6a56351ab56e
rongyi <sqrongyi@163.com>
2017-11-03 18:17:01 +0800

    Add leftRotate

---
+++ src/rotate-bits-left.cc
@@ -0,0 +1,12 @@
+// http://www.lintcode.com/zh-cn/problem/rotate-bits-left
+class Solution {
+public:
+  /*
+  * @param : a number
+  * @param : digit needed to be rorated
+  * @return: a number
+  */
+  int leftRotate(int n, int d) {
+
+  }
+};

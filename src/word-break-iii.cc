6d8f7868e0d57c2ba1b25dc72d98c530c25332b4
rongyi <sqrongyi@163.com>
2017-11-05 14:02:49 +0800

    Add wordBreak3

---
+++ src/word-break-iii.cc
@@ -0,0 +1,29 @@
+// http://www.lintcode.com/zh-cn/problem/word-break-iii
+#include <vector>
+#include <iostream>
+#include <string>
+#include <unordered_set>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+using std::unordered_set;
+
+class Solution {
+public:
+  /*
+  * @param : A string
+  * @param : A set of word
+  * @return: the number of possible sentences.
+  */
+  int wordBreak3(string& s, unordered_set<string>& dict) {
+
+  }
+};
+
+int main()
+{
+
+  return 0;
+}

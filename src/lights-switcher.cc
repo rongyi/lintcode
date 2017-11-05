e5374a9ecb755c2c5d30fb3494088fddd666900a
rongyi <sqrongyi@163.com>
2017-11-05 09:53:49 +0800

    Add flipLights

---
+++ src/lights-switcher.cc
@@ -0,0 +1,26 @@
+// http://www.lintcode.com/zh-cn/problem/lights-switcher
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /*
+  * @param : number of lights
+  * @param : number of operations
+  * @return: the number of status
+  */
+  int flipLights(int n, int m) {
+
+  }
+};
+
+int main() {
+
+  return 0;
+}

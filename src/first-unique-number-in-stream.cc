545eb9a939d6a861c7ef31d835d25becca53bb40
rongyi <sqrongyi@163.com>
2017-11-09 10:36:20 +0800

    Fuck, unordered_map is slow than map?

--- src/first-unique-number-in-stream.cc
+++ src/first-unique-number-in-stream.cc
@@ -6,6 +6,7 @@
 #include <vector>
 #include <unordered_set>
 #include <unordered_map>
+#include <map>
 #include <list>
 #include <algorithm>
 
@@ -16,12 +17,45 @@ using std::string;
 
 class Solution {
 public:
+  int firstUniqueNumber(vector<int> nums, int number) {
+    const int n = nums.size();
+    bool has_number = false;
+    int number_index = -1;
+
+    // number is not an index, it is a value in this vector
+    for (int i = 0; i < n; ++i) {
+      if (nums[i] == number) {
+        has_number = true;
+        number_index = i;
+        break;
+      }
+    }
+
+    if (!has_number) {
+      return -1;
+    }
+    std::map<int, int> index_map;
+    for (int i = 0; i <= number_index; i++) {
+      index_map[nums[i]] = i;
+    }
+
+    for (int i = 0; i <= number_index; ++i) {
+      if (index_map[nums[i]] == i) {
+        return nums[i];
+      } else {
+        index_map[nums[i]] = i;
+      }
+    }
+
+    return -1;
+  }
+
   /*
   * @param : a continuous stream of numbers
   * @param : a number
   * @return: returns the first unique number
   */
-  int firstUniqueNumber(vector<int> nums, int number) {
+  int firstUniqueNumberTLE(vector<int> nums, int number) {
     const int n = nums.size();
     bool has_number = false;
     int number_index = -1;

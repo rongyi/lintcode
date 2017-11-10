c3f21d6c8d5ffc7cbf74bc1a3691de117be3e036
rongyi <sqrongyi@163.com>
2017-11-10 08:23:58 +0800

    Add uniqueWeightedPaths

---
+++ src/unique-paths-iii.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/unique-paths-iii
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /*
+  * @param : an array of arrays
+  * @return: the sum of all unique weighted paths
+  */
+  int uniqueWeightedPaths(vector<vector<int>>& grid) {
+    // write your codes here
+  }
+};

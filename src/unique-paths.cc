64b9764f00759413bbd6743d85a6d04d1e366992
rongyi <sqrongyi@163.com>
2017-11-10 09:08:14 +0800

    Update uniquePaths

--- src/unique-paths.cc
+++ src/unique-paths.cc
@@ -24,15 +24,19 @@ public:
   }
 
   int uniquePaths(int m, int n) {
-    // 目的时让第一列和第一行都为1，内层的都会被改写
-    vector<vector<int>> dp(m, vector<int>(n, 1));
+    vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+    dp[1][1] = 1;
 
-    for (int i = 1; i < m; ++i) {
-      for (int j = 1; j < n; ++j) {
+    for (int i = 1; i <= m; ++i) {
+      for (int j = 1; j <= n; ++j) {
+        // ignore the origin point
+        if (i == 1 && j == 1) {
+          continue;
+        }
         dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
       }
     }
-    return dp[m-1][n-1];
+    return dp[m][n];
   }
 };
 

0c20b43de63aeadaea85b2e178ce6659a0aa5e30
rongyi <sqrongyi@163.com>
2017-11-12 09:52:10 +0800

    Add factorial

---
+++ src/factorial.cc
@@ -0,0 +1,20 @@
+// http://www.lintcode.com/zh-cn/problem/factorial
+#include <vector>
+#include <iostream>
+#include <string>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /*
+  * @param : an integer
+  * @return:  the factorial of n
+  */
+  string factorial(int n) {
+
+  }
+};

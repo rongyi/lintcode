6f9e0d577407dd55f05469544220c306b7d7ad5c
rongyi <sqrongyi@163.com>
2017-11-12 10:13:30 +0800

    TLE version of big factorial

--- src/factorial.cc
+++ src/factorial.cc
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <string>
 #include <limits>
+#include <algorithm>
 
 using std::vector;
 using std::cout;
@@ -22,7 +23,14 @@ public:
       return std::to_string(f);
     }
     // string multiply
-    return "";
+    auto ret = std::to_string(n);
+    for (int i = n - 1; i >= 1; --i) {
+      auto s2 = std::to_string(i);
+      auto multi = multiply(ret, s2);
+      ret = multi;
+    }
+
+    return ret;
   }
 private:
   int fac(int i) {
@@ -30,6 +38,71 @@ private:
       return 1;
     }
     return i * fac(i - 1);
+  }
+
+
+  /**
+   * @param num1 a non-negative integers
+   * @param num2 a non-negative integers
+   * @return return product of num1 and num2
+   */
+  string multiply(string &num1, string &num2) {
+    vector<string> ret;
+    for (int i = num2.size() - 1; i >= 0; i--) {
+      int carry = 0;
+      std::stringstream ss;
+
+      for (int j = num1.size() - 1; j >= 0; j--) {
+        int mulret = (num1[j] - '0') * (num2[i] - '0') + carry;
+        ss << (mulret % 10);
+        carry = mulret / 10;
+      }
+      if (carry != 0) {
+        ss << carry;
+      }
+      string cur_ret = ss.str();
+      std::reverse(cur_ret.begin(), cur_ret.end());
+      // shift
+      for (int k = 0; k < (num2.size() - i - 1); k++) {
+        cur_ret += "0";
+      }
+      ret.push_back(cur_ret);
+    }
+    string sum;
+    for (int i = 0; i < ret.size(); i++) {
+      sum = addStrings(sum, ret[i]);
+    }
+    // reduce zero,
+    // FIXME: may be write some continue stmt in the process above
+    if (std::count(sum.begin(), sum.end(), '0') == sum.size()) {
+      return "0";
+    }
+
+    return sum;
+  }
+
+private:
+  string addStrings(string &num1, string &num2) {
+    std::reverse(num1.begin(), num1.end());
+    std::reverse(num2.begin(), num2.end());
+    std::stringstream ss;
+    int len = std::max(num1.size(), num2.size());
+    int carry = 0;
+    for (int i = 0; i < len; i++) {
+      int cur1 = (i < num1.size() ? (num1[i] - '0') : 0);
+      int cur2 = (i < num2.size() ? (num2[i] - '0') : 0);
+      int sum = cur1 + cur2 + carry;
+      ss << (sum % 10);
+      carry = sum / 10;
+    }
+
+    if (carry != 0) {
+      ss << carry;
+    }
+
+    string ret = ss.str();
+    std::reverse(ret.begin(), ret.end());
+    return ret;
   }
 };
 

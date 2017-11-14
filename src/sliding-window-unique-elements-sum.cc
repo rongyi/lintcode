973cedd0ea637b79c5180dc4ddd486bc57885507
rongyi <sqrongyi@163.com>
2017-11-14 14:03:48 +0800

    Update slidingWindowUniqueElementsSum

---
+++ src/sliding-window-unique-elements-sum.cc
@@ -0,0 +1,22 @@
+// http://www.lintcode.com/zh-cn/problem/sliding-window-unique-elements-sum
+#include <bitset>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /*
+  * @param : the given array
+  * @param : the window size
+  * @return: the sum of the count of unique elements in each window
+  */
+  int slidingWindowUniqueElementsSum(vector<int> nums, int k) {
+
+  }
+};

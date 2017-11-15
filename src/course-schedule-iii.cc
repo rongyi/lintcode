9a93337e736f9e19eee75fec1544183ca26b9799
rongyi <sqrongyi@163.com>
2017-11-15 08:27:55 +0800

    Add scheduleCourse

---
+++ src/course-schedule-iii.cc
@@ -0,0 +1,22 @@
+// http://www.lintcode.com/zh-cn/problem/course-schedule-iii
+
+#include <bitset>
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Solution {
+public:
+  /*
+   * @param : duration and close day of each course
+   * @return: the maximal number of courses that can be taken
+   */
+  int scheduleCourse(vector<vector<int>> &courses) {
+
+  }
+};

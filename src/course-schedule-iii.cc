4d26a7bda5dd19cca310c5d2ad9ecc6b6b125690
rongyi <sqrongyi@163.com>
2017-11-15 09:31:48 +0800

    Update course scheduleCourse

--- src/course-schedule-iii.cc
+++ src/course-schedule-iii.cc
@@ -1,7 +1,9 @@
 // http://www.lintcode.com/zh-cn/problem/course-schedule-iii
 
+#include <algorithm>
 #include <bitset>
 #include <iostream>
+#include <queue>
 #include <string>
 #include <vector>
 
@@ -17,6 +19,28 @@ public:
    * @return: the maximal number of courses that can be taken
    */
   int scheduleCourse(vector<vector<int>> &courses) {
-
+    int cur_time = 0;
+    std::priority_queue<int> q;
+    std::sort(courses.begin(), courses.end(),
+              [](vector<int> &a, vector<int> &b) { return a[1] < b[1]; });
+    for (auto course : courses) {
+      cur_time += course[0];
+      q.push(course[0]);
+      if (cur_time > course[1]) {
+        cur_time -= q.top();
+        q.pop();
+      }
+    }
+    return q.size();
   }
 };
+
+int main()
+{
+  Solution so;
+  vector<vector<int>> test{{100, 200}, {200, 1300}, {1000, 1250}, {2000, 3200}};
+  auto ret = so.scheduleCourse(test);
+
+  cout << ret << endl;
+  return 0;
+}

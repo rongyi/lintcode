8a22fe253dae21169fe205b667ee48fa35375ea0
rongyi <sqrongyi@163.com>
2017-11-22 13:38:38 +0800

    Update sum of all subsets

--- src/sum-of-all-subsets.cc
+++ src/sum-of-all-subsets.cc
@@ -1,9 +1,9 @@
 // http://www.lintcode.com/zh-cn/problem/sum-of-all-subsets
-#include <vector>
+#include <algorithm>
+#include <bitset>
 #include <iostream>
 #include <string>
-#include <bitset>
-#include <algorithm>
+#include <vector>
 
 using std::vector;
 using std::cout;
@@ -17,9 +17,11 @@ public:
   * @return: Sum of elements in subsets
   */
   int subSum(int n) {
-    // For an input {x1, …, xn}, return 2n-1 (x1 + … + xn), since each term appears in that many sums.
+    // For an input {x1, …, xn}, return 2n-1 (x1 + … + xn), since each term
+    // appears in that many sums.
     return (1 << (n - 1)) * sum(n);
   }
+
 private:
   int sum(int n) {
     int ret = 0;

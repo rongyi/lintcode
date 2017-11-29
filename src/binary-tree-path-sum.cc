f617ba4541fc6aec7f582fa9412897424132f631
rongyi <sqrongyi@163.com>
2017-11-29 15:04:24 +0800

    Add another version of path sum

--- src/binary-tree-path-sum.cc
+++ src/binary-tree-path-sum.cc
@@ -39,13 +39,21 @@ public:
    * @param target an integer
    * @return all valid paths
    */
-  vector<vector<int>> binaryTreePathSum(TreeNode *root, int target) {
+  vector<vector<int>> binaryTreePathSumV1(TreeNode *root, int target) {
     vector<vector<int>> ret;
     if (root == nullptr)
       return ret;
 
     vector<int> aux;
     dfs(ret, aux, root, target);
+
+    return ret;
+  }
+  vector<vector<int>> binaryTreePathSum(TreeNode *root, int target) {
+    vector<vector<int>> ret;
+
+    vector<int> cur;
+    path_sum(root, target, cur, ret);
 
     return ret;
   }
@@ -59,6 +67,7 @@ private:
     return sum;
   }
 
+  // 这里 cur_sum_lst 是复制后调用，所以这里没有回朔的 pop_back 步骤
   void dfs(vector<vector<int>> &ret, vector<int> cur_sum_lst,
            const TreeNode *cur_node, const int target) {
     cur_sum_lst.push_back(cur_node->val);
@@ -75,5 +84,21 @@ private:
     if (cur_node->right) {
       dfs(ret, cur_sum_lst, cur_node->right, target);
     }
+  }
+
+  void path_sum(TreeNode *root, int gap, vector<int> &cur, vector<vector<int>> &ret) {
+    if (root == nullptr) {
+      return;
+    }
+    cur.push_back(root->val);
+
+    if (root->left == nullptr && root->right == nullptr) {
+      if (gap == root->val) {
+        ret.push_back(cur);
+      }
+    }
+    path_sum(root->left, gap - root->val, cur, ret);
+    path_sum(root->right, gap - root->val, cur, ret);
+    cur.pop_back();
   }
 };

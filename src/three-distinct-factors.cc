a2eca5e90b967ab5b7e9d884cfb508bdb768dea9
rongyi <sqrongyi@163.com>
2017-12-03 09:32:45 +0800

    AC three distinct factors

--- src/three-distinct-factors.cc
+++ src/three-distinct-factors.cc
@@ -1,6 +1,5 @@
 // http://www.lintcode.com/zh-cn/problem/three-distinct-factors
-// #include <climits>
-// #include <cctype>
+#include <cmath>
 class Solution {
 public:
   /*
@@ -24,6 +23,31 @@ public:
         } else {
           divided = true;
         }
+      }
+    }
+    return true;
+  }
+  bool isThreeDisctFactors(long long n) {
+    int sq = (int)std::sqrt(n);
+    if (1ll * sq * sq != n) {
+      return false;
+    }
+    return isPrime(sq) ? true : false;
+  }
+
+  bool isPrime(long long n) {
+    if (n <= 1) {
+      return false;
+    }
+    if (n <= 3) {
+      return true;
+    }
+    if (n % 2 == 0) {
+      return false;
+    }
+    for (int i = 2; i * i < n; ++i) {
+      if (n % i == 0) {
+        return false;
       }
     }
     return true;

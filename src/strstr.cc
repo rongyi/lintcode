9bcda114aee604f4bd7690a72c37ed05aaec17eb
rongyi <sqrongyi@163.com>
2017-12-06 14:11:22 +0800

    Update strstr

--- src/strstr.cc
+++ src/strstr.cc
@@ -1,4 +1,14 @@
 // http://www.lintcode.com/zh-cn/problem/strstr
+#include <vector>
+#include <iostream>
+#include <string>
+#include <bitset>
+
+using std::vector;
+using std::cout;
+using std::endl;
+using std::string;
+
 
 class Solution {
 public:
@@ -8,7 +18,7 @@ public:
    * @param source string to be scanned.
    * @param target string containing the sequence of characters to match.
    */
-  int strStr(const char *haystack, const char *needle) {
+  int strStr1(const char *haystack, const char *needle) {
     if (!haystack || !needle)
       return -1;
     // empty string needle
@@ -31,6 +41,31 @@ public:
         return p1_old - haystack;
 
       p1 = p1_old + 1;
+    }
+    return -1;
+  }
+
+  int strStr(const char *haystack, const char *needle) {
+    if (!haystack || !needle) {
+      return -1;
+    }
+    const string h(haystack);
+    const string n(needle);
+
+    if (n.empty()) {
+      return 0;
+    }
+    const int end = h.size() - n.size() + 1;
+    for (int i = 0; i < end; ++i) {
+      int j = i;
+      int k = 0;
+      while (j < h.size() && k < n.size() && h[j] == n[k]) {
+        j++;
+        k++;
+      }
+      if (k == n.size()) {
+        return i;
+      }
     }
     return -1;
   }

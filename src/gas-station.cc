d5fdd78c83c378da55d4023e62bf89d6a509f499
rongyi <sqrongyi@163.com>
2017-12-07 08:35:53 +0800

    Update gas station

--- src/gas-station.cc
+++ src/gas-station.cc
@@ -15,12 +15,14 @@ public:
     int j = -1;
     for (int i = 0, sum = 0; i < gas.size(); i++) {
       sum += gas[i] - cost[i];
+      // 不管从哪里开始兜一圈的油耗总和不变，
       total += gas[i] - cost[i];
+      // 表示从前面某个点开始到当前节点“够不着”
       if (sum < 0) {
         j = i; // the next possible index;
         sum = 0;
       }
     }
-    return total >= 0 ? j + 1 : -1;
+    return total >= 0 ? j + 1: -1;
   }
 };

bfb4c918885b5a94e681a87c13a9439f1336239b
rongyi <sqrongyi@163.com>
2017-12-16 09:57:31 +0800

    AC digitDivideNums

--- src/digit-divide-numbers.cc
+++ src/digit-divide-numbers.cc
@@ -1,8 +1,9 @@
 // http://www.lintcode.com/zh-cn/problem/digit-divide-numbers
 #include <iostream>
+#include <limits>
 #include <string>
-#include <vector>
 #include <unordered_set>
+#include <vector>
 
 using std::vector;
 using std::cout;
@@ -19,34 +20,35 @@ public:
   vector<int> digitDivideNums(int lower, int upper) {
     vector<int> ret;
     for (int i = lower; i <= upper; ++i) {
-
+      if (isdn(i)) {
+        ret.push_back(i);
+      }
+      // incase overflow
+      if (std::numeric_limits<int>::max() == i) {
+        break;
+      }
     }
+    return ret;
   }
 
-  bool isdn(int n) {
-    if (n == 0) {
-      return false;
-    } else if (n < 10) {
-      return true;
-    }
-    std::unordered_set<int> ds;
-    while (n) {
-      auto d = n % 10;
-      if (d == 0) {
-        return false;
-      }
-      ds.insert(d);
-      n /= 10;
-    }
-    if (ds.find(2) != ds.end() && (n & 0x1)) {
-      return false;
-    }
-    for (auto iter = ds.begin(); iter != ds.end(); ++iter) {
-      if (n % (*iter) != 0) {
+  // string process is fast than digit
+  bool isdn(long long num) {
+    string str = std::to_string(num);
+    for (char c : str) {
+      if (c == '0' || num % (c - '0')) {
         return false;
       }
     }
-
     return true;
   }
 };
+
+int main() {
+  Solution so;
+  auto ret = so.digitDivideNums(2147483000, 2147483647);
+  for (auto i : ret) {
+    cout << i << " ";
+  }
+  cout << endl;
+  return 0;
+}

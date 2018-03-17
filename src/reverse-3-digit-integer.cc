a3ca38664aa14d0897a8526a80fd314d10b39988
rongyi <sqrongyi@163.com>
2018-03-17 16:15:54 +0800

    Add reverseInteger template

---
+++ src/reverse-3-digit-integer.cc
@@ -0,0 +1,11 @@
+// http://www.lintcode.com/zh-cn/problem/reverse-3-digit-integer
+class Solution {
+public:
+  /**
+   * @param number: A 3-digit number.
+   * @return: Reversed number.
+   */
+  int reverseInteger(int number) {
+
+  }
+};

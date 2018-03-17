f2b8eb1011e55badc182a00d7b51be3ae67ee26c
rongyi <sqrongyi@163.com>
2018-03-17 17:01:04 +0800

    Add comment for getSum

--- src/and-and-or.cc
+++ src/and-and-or.cc
@@ -17,8 +17,11 @@ public:
    * @return: return the sum of maximum OR sum, minimum OR sum, maximum AND sum, minimum AND sum.
    */
   long long getSum(int n, vector<int> &nums) {
+    // or_min: select the min one is or min;
     auto or_min = *std::min_element(nums.begin(), nums.end());
+    // and_max: select the max one and it is the and_max
     auto and_max = *std::max_element(nums.begin(), nums.end());
+    // or_max add every one
     auto or_max = 0;
     for (int i = 0; i < n; i++) {
       or_max |= nums[i];

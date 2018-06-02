9866ccbe726a77e1c0259850d16f3a886d706f25
rongyi <sqrongyi@163.com>
2018-06-02 17:04:44 +0800

    Update tower of hanoi

--- src/tower-of-hanoi.cc
+++ src/tower-of-hanoi.cc
@@ -24,14 +24,14 @@ public:
   }
 
 private:
-  string cacat(char from, char to) {
+  string concat(char from, char to) {
     std::stringstream ss;
     ss << "from " << from << " to " << to;
     return ss.str();
   }
   void doHanoi(int n, char from, char to, char aux, vector<string> &ret) {
     if (n == 1) {
-      ret.push_back(cacat(from, to));
+      ret.push_back(concat(from, to));
       return;
     }
     // 实现这个算法可以简单分为三个步骤：
@@ -39,7 +39,7 @@ private:
     // （2）把第n个盘子由A移到C；
     // （3）把n-1个盘子由B移到C；
     doHanoi(n - 1, from, aux, to, ret);
-    ret.push_back(cacat(from, to));
+    ret.push_back(concat(from, to));
     doHanoi(n - 1, aux, to, from, ret);
   }
 };
